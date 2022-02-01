a96fcaeeae4d9871dd33120261cc4ead4ddce9d3
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-02-01 09:02:48 +0300

    Optimizing font manager

--- [Handler]/player_handler/settings/client.lua
+++ [Handler]/player_handler/settings/client.lua
@@ -8,17 +8,26 @@
 ----------------------------------------------------------------
 
 
+-----------------
+--[[ Imports ]]--
+-----------------
+
+local imports = {
+    dxCreateFont = dxCreateFont
+}
+
+
 ------------------
 --[[ Settings ]]--
 ------------------
 
 FRAMEWORK_FONTS = {
-    [1] = dxCreateFont("files/fonts/architectsdaughter.ttf", 17, true),
-    [2] = dxCreateFont("files/fonts/signika_semibold.ttf", 15, true),
-    [3] = dxCreateFont("files/fonts/courgette.ttf", 20, true),
-    [4] = dxCreateFont("files/fonts/signika_semibold.ttf", 12, true),
-    [5] = dxCreateFont("files/fonts/signika_semibold.ttf", 11, true),
-    [6] = dxCreateFont("files/fonts/architectsdaughter.ttf", 13, true),
-    [7] = DxFont("files/fonts/signika_semibold.ttf", 11, true),
-    [8] = DxFont("files/fonts/signika_semibold.ttf", 11, true)
+    [1] = imports.dxCreateFont("files/fonts/architectsdaughter.ttf", 17, true),
+    [2] = imports.dxCreateFont("files/fonts/signika_semibold.ttf", 15, true),
+    [3] = imports.dxCreateFont("files/fonts/courgette.ttf", 20, true),
+    [4] = imports.dxCreateFont("files/fonts/signika_semibold.ttf", 12, true),
+    [5] = imports.dxCreateFont("files/fonts/signika_semibold.ttf", 11, true),
+    [6] = imports.dxCreateFont("files/fonts/architectsdaughter.ttf", 13, true),
+    [7] = imports.dxCreateFont("files/fonts/signika_semibold.ttf", 11, true),
+    [8] = imports.dxCreateFont("files/fonts/signika_semibold.ttf", 11, true)
 }
