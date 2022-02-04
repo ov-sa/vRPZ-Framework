8cf917630bc60085b8fc22f43df008f20a23e97a
Aviril <89867985+Aviril@users.noreply.github.com>
2022-02-04 12:13:36 +0300

    Updated

--- [Handler]/player_handler/exports/shared/main.lua
+++ [Handler]/player_handler/exports/shared/main.lua
@@ -344,3 +344,33 @@ function getBackpackOffset(skinModel, backpackType)
     return false
 
 end
+
+
+--------------------------------------------------------
+--[[ Function: Retrieves Player's Current Slot Item ]]--
+--------------------------------------------------------
+
+function getPlayerCurrentSlotItem(player, slotType)
+
+    if not player or not isElement(player) or player:getType() ~= "player" or not slotType then return false end
+    if not isPlayerInitialized(player) then return false end
+
+    if slotType == "backpack" then
+        local item = player:getData("Slot:backpack")
+        if getItemDetails(item) then
+            return item
+        end
+    elseif slotType == "weapon" then
+        local playerWeapon = player:getWeapon()
+        for i, j in pairs(availableWeaponSlots) do
+            local weaponName = tostring(player:getData("Slot:"..i))
+            for k, v in ipairs(inventoryDatas[(characterSlots[i].slotCategory)]) do
+                if (v.weaponID == playerWeapon) and (v.dataName == weaponName) then
+                    return weaponName, i
+                end
+            end
+        end
+    end
+    return false
+
+end
