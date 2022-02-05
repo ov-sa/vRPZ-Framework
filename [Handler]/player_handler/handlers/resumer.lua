92ff0e2c500843bfdbe48589a5e613acd2654d3e
Aviril <89867985+Aviril@users.noreply.github.com>
2022-02-05 09:10:02 +0300

    Update resumer.lua

--- [Handler]/player_handler/handlers/resumer.lua
+++ [Handler]/player_handler/handlers/resumer.lua
@@ -49,7 +49,7 @@ imports.addEventHandler("Player:onSaveCharacter", root, function(characterID, ch
     for i, j in imports.pairs(unsavedChars) do
         if characters[i] then
             for k, v in imports.pairs(characterCache) do
-                if v["identity"]["name"] == characters[i]["name"] then
+                if v.identity["name"] == characters[i]["name"] then
                     imports.triggerClientEvent(source, "onClientLoginUIEnable", source, true, true)
                     imports.triggerClientEvent(source, "onClientRecieveCharacterSaveState", source, false, "Unfortunately, '"..characters[i]["name"].."' is already registered!", i)
                     return false
@@ -57,7 +57,7 @@ imports.addEventHandler("Player:onSaveCharacter", root, function(characterID, ch
             end
             local characterID = CCharacter.create(serial, function(characterID, Args)
                 CCharacter.setData(characterID, "identity", imports.toJSON(characters[i]))
-                characterCache[characterID]["identity"] = fromJSON(characterCache[characterID]["identity"])
+                characterCache[characterID].identity = fromJSON(characterCache[characterID].identity)
                 imports.triggerClientEvent(Args[1], "onClientLoadCharacterID", Args[1], i, characterID, characters[i])
             end, source)
         end
@@ -90,7 +90,7 @@ imports.addEventHandler("Player:onToggleLoginUI", root, function()
                 Args[2].characters = result
                 for i = 1, #Args[2].characters, 1 do
                     local j = Args[2].characters[i]
-                    j["identity"] = imports.fromJSON(j["identity"])
+                    j.identity = imports.fromJSON(j.identity)
                 end
                 if not Args[2].characters[(Args[2].character)] then Args[2].character = 0 end
                 imports.triggerClientEvent(Args[1], "Client:onToggleLoadingUI", Args[1], {
