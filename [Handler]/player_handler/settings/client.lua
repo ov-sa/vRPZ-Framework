6bb153c8dc995bccf2a065c9aa285a74bb9d7121
Aviril <89867985+Aviril@users.noreply.github.com>
2022-02-13 13:59:01 +0300

    Update client.lua

--- [Handler]/player_handler/settings/client.lua
+++ [Handler]/player_handler/settings/client.lua
@@ -13,7 +13,7 @@
 -----------------
 
 local imports = {
-    dxCreateFont = dxCreateFont
+    beautify = beautify
 }
 
 
@@ -23,19 +23,7 @@ local imports = {
 
 FRAMEWORK_LANGUAGE = "EN"
 FRAMEWORK_FONTS = {
-    [1] = imports.dxCreateFont(":beautify_library/files/assets/fonts/teko_medium.rw", 23),
-    [2] = imports.dxCreateFont(":beautify_library/files/assets/fonts/teko_medium.rw", 30),
-    [3] = imports.dxCreateFont(":beautify_library/files/assets/fonts/teko_medium.rw", 30, true),
-
-
-    --TODO: ALL THIS NEEDS TO BE MODIFIED TOO
-    --[2] = imports.dxCreateFont("files/fonts/signika_semibold.ttf", 15, true),
-    --[2] = imports.dxCreateFont("files/fonts/insomnia.ttf", 17),
-    --[3] = imports.dxCreateFont("files/fonts/courgette.ttf", 20, true),
-
-    [4] = imports.dxCreateFont("files/fonts/signika_semibold.ttf", 12, true),
-    [5] = imports.dxCreateFont("files/fonts/signika_semibold.ttf", 11, true),
-    [6] = imports.dxCreateFont("files/fonts/architectsdaughter.ttf", 13, true),
-    [7] = imports.dxCreateFont("files/fonts/signika_semibold.ttf", 11, true),
-    [8] = imports.dxCreateFont("files/fonts/signika_semibold.ttf", 11, true)
+    [1] = imports.beautify.native.createFont(":beautify_library/files/assets/fonts/teko_medium.rw", 23),
+    [2] = imports.beautify.native.createFont(":beautify_library/files/assets/fonts/teko_medium.rw", 30),
+    [3] = imports.beautify.native.createFont(":beautify_library/files/assets/fonts/teko_medium.rw", 30)
 }
