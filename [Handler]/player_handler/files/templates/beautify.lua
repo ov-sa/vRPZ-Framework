3026fc7289dabf40ab871789c7a3246cc071c910
Aviril <89867985+Aviril@users.noreply.github.com>
2022-02-13 13:17:22 +0300

    Merged Maria's more templates

--- [Handler]/player_handler/files/templates/beautify.lua
+++ [Handler]/player_handler/files/templates/beautify.lua
@@ -1,5 +1,39 @@
-BEAUTIFY_TEMPLATE = {
+-----------------
+--[[ Imports ]]--
+-----------------
+
+local imports = {
+    pairs = pairs,
+    beautify = beautify
+}
+
+
+-------------------
+--[[ Templates ]]--
+-------------------
+
+local BEAUTIFY_TEMPLATE = {
     ["beautify_card"] = {
         color = {0, 0, 0, 0}
+    },
+
+    ["beautify_selector"] = {
+        fontPaddingY = 0,
+        font = {"files/assets/fonts/teko_medium.rw", 16, "beautify_library"},
+        color = {100, 100, 100, 255},
+        fontColor = {200, 200, 200, 255},
+        hoverColor = {200, 200, 200, 255}
+    },
+
+    ["beautify_selector"] = {
+        fontPaddingY = 0,
+        font = {"files/assets/fonts/teko_medium.rw", 16, "beautify_library"},
+        color = {100, 100, 100, 255},
+        fontColor = {200, 200, 200, 255},
+        hoverColor = {200, 200, 200, 255}
     }
 }
+
+for i, j in imports.pairs(BEAUTIFY_TEMPLATE) do
+    imports.beautify.setUITemplate(i, j)
+end
