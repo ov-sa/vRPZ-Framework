12660e8395c9e2f0be11264f707d94ecf106cfdc
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-02-14 18:25:06 +0300

    Updated

--- [Library]/assetify_library/utilities/shaders/loader.lua
+++ [Library]/assetify_library/utilities/shaders/loader.lua
@@ -1,11 +1,11 @@
 ----------------------------------------------------------------
 --[[ Resource: Assetify Library
-     Script: utilities: shaders: initial.lua
+     Script: utilities: shaders: loader.lua
      Server: -
      Author: OvileAmriam
      Developer(s): Aviril, Tron
      DOC: 19/10/2021 (OvileAmriam)
-     Desc: Shader Initializer ]]--
+     Desc: Shader Loader ]]--
 ----------------------------------------------------------------
 
 
