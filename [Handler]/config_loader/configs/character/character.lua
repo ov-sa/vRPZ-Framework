3d2e65a8f9eeaa446a2032bc2c45c5bbafd49df8
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-02-23 02:29:55 +0300

    Finalized config

--- [Handler]/config_loader/configs/character/character.lua
+++ [Handler]/config_loader/configs/character/character.lua
@@ -53,33 +53,28 @@ configVars["Character"] = {
                 {clumpName = "TS1", clumpTexture = {"cj_tshrt_1", 11}, ["EN"] = "T-Shirt (ZM#1)", ["TR"] = "Tişört (ZM#1)"},
                 {clumpName = "TS1", clumpTexture = {"cj_tshrt_1", 12}, ["EN"] = "T-Shirt (ZM#2)", ["TR"] = "Tişört (ZM#2)"},
                 {clumpName = "TS1", clumpTexture = {"cj_tshrt_1", 13}, ["EN"] = "T-Shirt (ZM#3)", ["TR"] = "Tişört (ZM#3)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 1}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 2}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 3}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 4}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 5}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 6}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 7}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 8}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 9}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 10}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 11}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 12}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 13}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 14}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 15}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 16}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 17}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 18}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 19}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 20}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 21}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"}
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 1}, ["EN"] = "Hoody (VRPZ#1)", ["TR"] = "Kapüşonlu (VRPZ#1)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 2}, ["EN"] = "Hoody (VRPZ#2)", ["TR"] = "Kapüşonlu (VRPZ#2)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 3}, ["EN"] = "Hoody (VRPZ#3)", ["TR"] = "Kapüşonlu (VRPZ#3)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 4}, ["EN"] = "Hoody (VRPZ#4)", ["TR"] = "Kapüşonlu (VRPZ#4)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 5}, ["EN"] = "Hoody (VRPZ#5)", ["TR"] = "Kapüşonlu (VRPZ#5)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 6}, ["EN"] = "Hoody (VRPZ#6)", ["TR"] = "Kapüşonlu (VRPZ#6)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 7}, ["EN"] = "Hoody (VRPZ#7)", ["TR"] = "Kapüşonlu (VRPZ#7)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 8}, ["EN"] = "Hoody (Camo#1)", ["TR"] = "Kapüşonlu (Camo#1)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 9}, ["EN"] = "Hoody (Camo#2)", ["TR"] = "Kapüşonlu (Camo#2)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 10}, ["EN"] = "Hoody (Camo#3)", ["TR"] = "Kapüşonlu (Camo#3)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 11}, ["EN"] = "Hoody (Camo#4)", ["TR"] = "Kapüşonlu (Camo#4)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 12}, ["EN"] = "Hoody (Camo#5)", ["TR"] = "Kapüşonlu (Camo#5)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 13}, ["EN"] = "Hoody (Camo#6)", ["TR"] = "Kapüşonlu (Camo#6)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 14}, ["EN"] = "Hoody (Camo#7)", ["TR"] = "Kapüşonlu (Camo#7)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 15}, ["EN"] = "Hoody (Camo#8)", ["TR"] = "Kapüşonlu (Camo#8)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 16}, ["EN"] = "Hoody (Camo#9)", ["TR"] = "Kapüşonlu (Camo#9)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 17}, ["EN"] = "Hoody (CS#1)", ["TR"] = "Kapüşonlu (CS#1)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 18}, ["EN"] = "Hoody (CS#2)", ["TR"] = "Kapüşonlu (CS#2)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 19}, ["EN"] = "Hoody (CS#3)", ["TR"] = "Kapüşonlu (CS#3)"},
+                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 20}, ["EN"] = "Hoody (CS#4)", ["TR"] = "Kapüşonlu (CS#4)"}
             },
-            ["Female"] = {
-                {clumpName = "TS1", clumpTexture = {"cj_tshrt_1", 1}, ["EN"] = "T-Shirt (LS)", ["TR"] = "Tişört (LS)"},
-                {clumpName = "TS1", clumpTexture = {"cj_tshrt_1", 2}, ["EN"] = "T-Shirt (LS)", ["TR"] = "Tişört (LS)"},
-                {clumpName = "H1", clumpTexture = {"cj_hdy_1", 1}, ["EN"] = "Hoody (Green)", ["TR"] = "Kapüşonlu (Yeşil)"}
-            }
+            ["Female"] = {}
         },
         ["Lower"] = {
             ["Male"] = {
@@ -95,20 +90,14 @@ configVars["Character"] = {
                 {clumpName = "P1", clumpTexture = {"cj_pnt_1", 10}, ["EN"] = "Pant (Camo#5)", ["TR"] = "Pantolon (Camo#5)"},
                 {clumpName = "T1", clumpTexture = {"cj_trsr_1", 1}, ["EN"] = "Trouser (Green)", ["TR"] = "Pantolon (Yeşil)"}
             },
-            ["Female"] = {
-                {clumpName = "P1", clumpTexture = {"cj_pnt_1", 1}, ["EN"] = "Jeans (Red)", ["TR"] = "Kot (Kırmızı)"},
-                {clumpName = "T1", clumpTexture = {"cj_trsr_1", 1}, ["EN"] = "Trouser (Green)", ["TR"] = "Pantolon (Yeşil)"}
-            }
+            ["Female"] = {}
         },
         ["Shoes"] = {
             ["Male"] = {
                 {clumpName = "", clumpTexture = {"cj_shoe_1", 1}, ["EN"] = "Sneakers (Black)", ["TR"] = "Spor Ayakkabı (Siyah)"},
                 {clumpName = "", clumpTexture = {"cj_shoe_1", 2}, ["EN"] = "Sneakers (Red)", ["TR"] = "Spor Ayakkabı (Red)"}
             },
-            ["Female"] = {
-                {clumpName = "", clumpTexture = {"cj_shoe_1", 1}, ["EN"] = "Sneakers (Black)", ["TR"] = "Spor Ayakkabı (Siyah)"},
-                {clumpName = "", clumpTexture = {"cj_shoe_1", 2}, ["EN"] = "Sneakers (Red)", ["TR"] = "Spor Ayakkabı (Red)"}
-            }
+            ["Female"] = {}
         }
     }
 
