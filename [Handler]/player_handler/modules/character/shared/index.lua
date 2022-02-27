a12864cdfcc37241fb97735a4375516e1930490d
Aviril <89867985+Aviril@users.noreply.github.com>
2022-02-27 08:33:20 +0300

    Update index.lua

--- [Handler]/player_handler/modules/character/shared/index.lua
+++ [Handler]/player_handler/modules/character/shared/index.lua
@@ -55,11 +55,11 @@ CCharacter = {
     generateClothing = function(characterIdentity)
         if not characterIdentity then return false end
         local clothingData = {
-            gender = FRAMEWORK_CONFIGS["UI"]["Login"]["Options"].characters.categories["Identity"].gender["Datas"][(characterIdentity.gender)],
-            hair = FRAMEWORK_CONFIGS["UI"]["Login"]["Options"].characters.categories["Facial"].hair["Datas"][(characterIdentity.gender)][(characterIdentity.hair)],
-            upper = FRAMEWORK_CONFIGS["UI"]["Login"]["Options"].characters.categories["Upper"]["Datas"][(characterIdentity.gender)][((characterIdentity.upper)],
-            lower = FRAMEWORK_CONFIGS["UI"]["Login"]["Options"].characters.categories["Lower"]["Datas"][(characterIdentity.gender)][((characterIdentity.lower)],
-            shoes = FRAMEWORK_CONFIGS["UI"]["Login"]["Options"].characters.categories["Shoes"]["Datas"][(characterIdentity.gender)][((characterIdentity.shoes)]
+            gender = FRAMEWORK_CONFIGS["Character"]["Identity"]["Gender"][(characterIdentity.gender)],
+            hair = FRAMEWORK_CONFIGS["Character"]["Clothing"]["Facial"]["Hair"][(characterIdentity.gender)][(characterIdentity.hair)],
+            upper = FRAMEWORK_CONFIGS["Character"]["Clothing"]["Upper"][(characterIdentity.gender)][((characterIdentity.upper)],
+            lower = FRAMEWORK_CONFIGS["Character"]["Clothing"]["Lower"][(characterIdentity.gender)][((characterIdentity.lower)],
+            shoes = FRAMEWORK_CONFIGS["Character"]["Clothing"]["Shoes"][(characterIdentity.gender)][((characterIdentity.shoes)]
         }
         return clothingData.gender.assetName, (clothingData.upper.clumpName)..(clothingData.lower.clumpName)..(clothingData.shoes.clumpName), {
             [(clothingData.upper.clumpTexture[1])] = clothingData.upper.clumpTexture[2],
