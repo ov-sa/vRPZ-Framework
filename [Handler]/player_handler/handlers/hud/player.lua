7ea3af978ad51e6188dd1430c7484dfad1566c24
Aviril <89867985+Aviril@users.noreply.github.com>
2022-02-27 18:26:39 +0300

    VG

--- [Handler]/player_handler/handlers/hud/player.lua
+++ [Handler]/player_handler/handlers/hud/player.lua
@@ -13,6 +13,7 @@
 -----------------
 
 local imports = {
+    tocolor = tocolor,
     beautify = beautify
 }
 
@@ -21,20 +22,44 @@ local imports = {
 --[[ Variables ]]--
 -------------------
 
-local hudUI = {
-    startX = 20, startY = 60,
-    width = 325, height = 75,
-    bgTexture = "texturePath",
-    bgColor = tocolor(255, 255, 255, 255)
+local cache = {
+    vignette = {
+        bgTexture = imports.beautify.native.createTexture("files/images/hud/overlays/vignette.png", "dxt5", true, "clamp"), bgColor = imports.tocolor(0, 0, 0, 255)
+    },
+
+    startX = 5, startY = 50,
+    smallSlotWidth = 70,
+    bgTexture = imports.beautify.native.createTexture("files/images/hud/player/slotBG1.png", "dxt5", true, "clamp"),
+    bgColor = tocolor(125, 125, 125, 150),
+    primary = {
+        width = 175, height = 53,
+        bgTexture = imports.beautify.native.createTexture("files/images/hud/player/slotBG1.png", "dxt5", true, "clamp"),
+    },
+    secondary = {
+        width = 100*0.75,
+        height = 50*0.75,
+        bgTexture = imports.beautify.native.createTexture("files/images/hud/player/slotBG2.png", "dxt5", true, "clamp")
+    },
+
 }
-hudUI.startX, hudUI.startY = CLIENT_MTA_RESOLUTION[1] - (hudUI.startX + hudUI.width), CLIENT_MTA_RESOLUTION[2] - (hudUI.startY + hudUI.height)
+cache.startX, cache.startY = CLIENT_MTA_RESOLUTION[1] - (cache.startX + cache.primary.width), CLIENT_MTA_RESOLUTION[2] - (cache.startY + cache.primary.height)
 
 
----------------------
---[[ Renders HUD ]]--
----------------------
+--------------------------------
+--[[ Functions: Renders HUD ]]--
+--------------------------------
+
+beautify.render.create(function()
+    if not CPlayer.isInitialized(localPlayer) then return false end
+    imports.beautify.native.drawImage(0, 0, CLIENT_MTA_RESOLUTION[1], CLIENT_MTA_RESOLUTION[2], cache.vignette.bgTexture, 0, 0, 0, cache.vignette.bgColor, false)
+end, {
+    renderType = "input"
+})
 
 beautify.render.create(function()
     --TODO: DRAW HUD HERE...
-    imports.beautify.native.drawRectangle(hudUI.startX, hudUI.startY, hudUI.width, hudUI.height, hudUI.bgColor, false)
+    imports.beautify.native.drawImage(cache.startX, cache.startY, cache.primary.width, cache.primary.height, cache.primary.bgTexture, 0, 0, 0, cache.bgColor, false)
+    imports.beautify.native.drawImage(cache.startX + cache.primary.width - cache.secondary.width, cache.startY - cache.secondary.height - 10, cache.secondary.width, cache.secondary.height, cache.secondary.bgTexture, 0, 0, 0, cache.bgColor, false)
+    --imports.beautify.native.drawRectangle(cache.startX + cache.width - cache.smallSlotWidth, cache.startY, cache.smallSlotWidth, cache.height, cache.bgColor, false)
+    --imports.beautify.native.drawImage(background_offsetX, background_offsetY, background_width, background_height, cache.bgTexture, 0, 0, 0, -1, false)
 end)
