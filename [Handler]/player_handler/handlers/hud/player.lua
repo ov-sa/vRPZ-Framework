6f650dab9a94823ba7bae11277124ef25277949a
Aviril <89867985+Aviril@users.noreply.github.com>
2022-03-01 04:15:28 +0300

    Update player.lua

--- [Handler]/player_handler/handlers/hud/player.lua
+++ [Handler]/player_handler/handlers/hud/player.lua
@@ -23,32 +23,44 @@ local imports = {
 -------------------
 
 local cache = {
+    startX = 5, startY = 50, padding = 10,
     vignette = {
         bgTexture = imports.beautify.native.createTexture("files/images/hud/overlays/vignette.png", "dxt5", true, "clamp"), bgColor = imports.tocolor(0, 0, 0, 255)
     },
-
-    startX = 5, startY = 50,
-    smallSlotWidth = 70,
-    bgTexture = imports.beautify.native.createTexture("files/images/hud/player/slotBG1.png", "dxt5", true, "clamp"),
-    bgColor = tocolor(125, 125, 125, 150),
     primary = {
         paddingX = 5, paddingY = 0,
         width = 175, height = 53,
-        font = FRAMEWORK_FONTS[7], fontColor = imports.tocolor(255, 255, 255, 255),
-        altfont = FRAMEWORK_FONTS[8], altfontColor = imports.tocolor(150, 150, 150, 255),
         bgTexture = imports.beautify.native.createTexture("files/images/hud/player/slotBG1.png", "dxt5", true, "clamp"),
+        ammo = {
+            font = FRAMEWORK_FONTS[7], fontColor = imports.tocolor(255, 255, 255, 255),
+            mag = {
+                font = FRAMEWORK_FONTS[8], fontColor = imports.tocolor(150, 150, 150, 255)
+            }
+        }
     },
     secondary = {
         paddingX = 5, paddingY = 0,
-        width = 100*0.75,
-        height = 50*0.75,
-        font = FRAMEWORK_FONTS[7], fontColor = imports.tocolor(255, 255, 255, 255),
-        altfont = FRAMEWORK_FONTS[8], altfontColor = imports.tocolor(150, 150, 150, 255),
-        bgTexture = imports.beautify.native.createTexture("files/images/hud/player/slotBG2.png", "dxt5", true, "clamp")
-    },
-
+        width = 100*0.75, height = 50*0.75,
+        bgTexture = imports.beautify.native.createTexture("files/images/hud/player/slotBG2.png", "dxt5", true, "clamp"),
+        ammo = {
+            font = FRAMEWORK_FONTS[7], fontColor = imports.tocolor(255, 255, 255, 255),
+            mag = {
+                font = FRAMEWORK_FONTS[8], fontColor = imports.tocolor(150, 150, 150, 255)
+            }
+        }
+    }
 }
-cache.startX, cache.startY = CLIENT_MTA_RESOLUTION[1] - (cache.startX + cache.primary.width), CLIENT_MTA_RESOLUTION[2] - (cache.startY + cache.primary.height)
+
+cache.primary.startX, cache.primary.startY = CLIENT_MTA_RESOLUTION[1] - (cache.startX + cache.primary.width), CLIENT_MTA_RESOLUTION[2] - (cache.startY + cache.primary.height)
+cache.secondary.startX, cache.secondary.startY = cache.primary.startX, cache.primary.startY - cache.padding
+cache.primary.ammo.startX, cache.primary.ammo.startY = cache.primary.startX + cache.primary.paddingX, cache.primary.startY
+cache.primary.ammo.endX, cache.primary.ammo.endY = cache.primary.ammo.startX, cache.primary.startY + cache.primary.height + cache.primary.paddingY
+cache.primary.ammo.mag.startX, cache.primary.ammo.mag.startY = cache.primary.ammo.startX + cache.primary.paddingX, cache.primary.ammo.startY
+cache.primary.ammo.mag.endX, cache.primary.ammo.mag.endY = cache.primary.ammo.mag.startX, cache.primary.ammo.endY
+cache.secondary.ammo.startX, cache.secondary.ammo.startY = cache.secondary.startX + cache.secondary.paddingX, cache.secondary.startY
+cache.secondary.ammo.endX, cache.secondary.ammo.endY = cache.secondary.ammo.startX, cache.secondary.startY + cache.secondary.height + cache.secondary.paddingY
+cache.secondary.ammo.mag.startX, cache.secondary.ammo.mag.startY = cache.secondary.ammo.startX + cache.primary.paddingX, cache.secondary.ammo.startY
+cache.secondary.ammo.mag.endX, cache.secondary.ammo.mag.endY = cache.secondary.ammo.mag.startX, cache.secondary.ammo.endY
 
 
 --------------------------------
@@ -63,17 +75,13 @@ end, {
 })
 
 beautify.render.create(function()
-    --TODO: DRAW HUD HERE...
-    imports.beautify.native.drawImage(cache.startX, cache.startY, cache.primary.width, cache.primary.height, cache.primary.bgTexture, 0, 0, 0, cache.bgColor, false)
-    imports.beautify.native.drawText("01", cache.startX, cache.startY, cache.startX + cache.primary.paddingX, cache.startY + cache.primary.height + cache.primary.paddingY, cache.primary.fontColor, 1, cache.primary.font, "right", "bottom", false, false, false)
-    imports.beautify.native.drawText("999", cache.startX + (cache.primary.paddingX*2), cache.startY, cache.startX + (cache.primary.paddingX*2), cache.startY + cache.primary.height + cache.primary.paddingY, cache.primary.altfontColor, 1, cache.primary.altfont, "left", "bottom", false, false, false)
-
-    local startX, startY = cache.startX + cache.primary.width - cache.secondary.width, cache.startY - cache.secondary.height - 10
-    local width, height = cache.secondary.width, cache.secondary.height
-    imports.beautify.native.drawImage(startX, startY, width, height, cache.secondary.bgTexture, 0, 0, 0, cache.bgColor, false)
-    imports.beautify.native.drawText("01", startX, startY, startX + cache.primary.paddingX, startY + height + cache.secondary.paddingY, cache.secondary.fontColor, 1, cache.secondary.font, "right", "bottom", false, false, false)
-    imports.beautify.native.drawText("999", startX + (cache.primary.paddingX*2), startY, startX + (cache.primary.paddingX*2), startY + height + cache.primary.paddingY, cache.primary.altfontColor, 1, cache.primary.altfont, "left", "bottom", false, false, false)
-
-    --imports.beautify.native.drawRectangle(cache.startX + cache.width - cache.smallSlotWidth, cache.startY, cache.smallSlotWidth, cache.height, cache.bgColor, false)
-    --imports.beautify.native.drawImage(background_offsetX, background_offsetY, background_width, background_height, cache.bgTexture, 0, 0, 0, -1, false)
+    --Primary Equipment--
+    imports.beautify.native.drawImage(cache.primary.startX, cache.primary.startY, cache.primary.width, cache.primary.height, cache.primary.bgTexture, 0, 0, 0, cache.bgColor, false)
+    imports.beautify.native.drawText("01", cache.primary.ammo.startX, cache.primary.ammo.startY, cache.primary.ammo.endX, cache.primary.ammo.endY, cache.primary.ammo.fontColor, 1, cache.primary.ammo.font, "right", "bottom", false, false, false)
+    imports.beautify.native.drawText("999", cache.primary.ammo.mag.startX, cache.primary.ammo.mag.startY, cache.primary.ammo.mag.endX, cache.primary.ammo.mag.endY, cache.primary.ammo.mag.fontColor, 1, cache.primary.ammo.mag.font, "left", "bottom", false, false, false)
+    
+    --Secondary Equipment--
+    imports.beautify.native.drawImage(cache.secondary.startX, cache.secondary.startY, cache.secondary.width, cache.secondary.height, cache.secondary.bgTexture, 0, 0, 0, cache.bgColor, false)
+    imports.beautify.native.drawText("01", cache.secondary.ammo.startX, cache.secondary.ammo.startY, cache.secondary.ammo.endX, cache.secondary.ammo.endY, cache.secondary.ammo.fontColor, 1, cache.secondary.ammo.font, "right", "bottom", false, false, false)
+    imports.beautify.native.drawText("999", cache.secondary.ammo.mag.startX, cache.secondary.ammo.mag.startY, cache.secondary.ammo.mag.endX, cache.secondary.ammo.mag.endY, cache.secondary.ammo.mag.fontColor, 1, cache.secondary.ammo.mag.font, "left", "bottom", false, false, false)
 end)
