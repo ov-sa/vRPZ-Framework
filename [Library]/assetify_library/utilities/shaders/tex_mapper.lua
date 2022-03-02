b114046bbbd29611c997e1bf524ba3681c83cfb9
Aviril <89867985+Aviril@users.noreply.github.com>
2022-03-02 07:03:06 +0300

    Update tex_mapper.lua

--- [Library]/assetify_library/utilities/shaders/tex_mapper.lua
+++ [Library]/assetify_library/utilities/shaders/tex_mapper.lua
@@ -39,12 +39,90 @@ end
 
 shaderRW[identifier] = function()
     return depDatas..[[
+    int gCapsMaxAnisotropy <string deviceCaps="MaxAnisotropy";>;
     /*-----------------
     -->> Variables <<--
     -------------------*/
 
     texture baseTexture;
 
+    bool enableBumpMap = false;
+    texture bumpTexture;
+    float anisotropy = 1;
+    float redControlScale = 1;
+    float greenControlScale = 1;
+    float blueControlScale = 1;
+    texture redControlTexture;
+    texture greenControlTexture;
+    texture blueControlTexture;
+    struct PSInput {
+        float4 Position : POSITION0;
+        float4 Diffuse : COLOR0;
+        float2 TexCoord : TEXCOORD0;
+    };
+
+    /*----------------
+    -->> Samplers <<--
+    ------------------*/
+
+    sampler controlSampler = sampler_state {
+        Texture = (gTexture0);
+        MipFilter = Linear;
+        MaxAnisotropy = gCapsMaxAnisotropy*anisotropy;
+        MinFilter = Anisotropic;
+    };
+    sampler redControlSampler = sampler_state { 
+        Texture = (redControlTexture);
+        MipFilter = Linear;
+        MaxAnisotropy = gCapsMaxAnisotropy*anisotropy;
+        MinFilter = Anisotropic;
+    };
+    sampler greenControlSampler = sampler_state { 
+        Texture = (greenControlTexture);
+        MipFilter = Linear;
+        MaxAnisotropy = gCapsMaxAnisotropy*anisotropy;
+        MinFilter = Anisotropic;
+    };
+    sampler blueControlSampler = sampler_state { 
+        Texture = (blueControlTexture);
+        MipFilter = Linear;
+        MaxAnisotropy = gCapsMaxAnisotropy*anisotropy;
+        MinFilter = Anisotropic;
+    }; 
+    sampler bumpSampler = sampler_state {
+        Texture = (bumpTexture);
+        MinFilter = Linear;
+        MagFilter = Linear;
+        MipFilter = Linear;
+    };
+
+
+    /*----------------
+    -->> Handlers <<--
+    ------------------*/
+    float4 PixelShaderFunction(PSInput PS) {
+        float4 controlTexel = tex2D(controlSampler, PS.TexCoord);
+        float4 redTexel = tex2D(redControlSampler, PS.TexCoord*redControlScale);
+        float4 greenTexel = tex2D(greenControlSampler, PS.TexCoord*greenControlScale);
+        float4 blueTexel = tex2D(blueControlSampler, PS.TexCoord*blueControlScale);
+        float4 sampledControlTexel = lerp(controlTexel, redTexel, controlTexel.r);
+        sampledControlTexel = lerp(controlTexel, redTexel, controlTexel.r);
+        sampledControlTexel = lerp(controlTexel, redTexel, controlTexel.r);
+        sampledControlTexel = lerp(sampledControlTexel, greenTexel, controlTexel.g);
+        sampledControlTexel = lerp(sampledControlTexel, greenTexel, controlTexel.g);
+        sampledControlTexel = lerp(sampledControlTexel, greenTexel, controlTexel.g);
+        sampledControlTexel = lerp(sampledControlTexel, blueTexel, controlTexel.b);
+        sampledControlTexel = lerp(sampledControlTexel, blueTexel, controlTexel.b);
+        sampledControlTexel = lerp(sampledControlTexel, blueTexel, controlTexel.b);
+        sampledControlTexel.rgb = sampledControlTexel.rgb/3;
+        if (enableBumpMap) {
+            float4 bumpTexel = tex2D(bumpSampler, PS.TexCoord);
+            sampledControlTexel.rgb *= bumpTexel.rgb;
+        }
+        sampledControlTexel.a = controlTexel.a;
+        sampledControlTexel = saturate(sampledControlTexel);
+        return sampledControlTexel;
+    }
 
     /*------------------
     -->> Techniques <<--
@@ -55,7 +133,8 @@ shaderRW[identifier] = function()
         pass P0
         {
             AlphaBlendEnable = true;
-            Texture[0] = baseTexture;
+            SRGBWriteEnable = false;
+            PixelShader = compile ps_2_0 PixelShaderFunction();
         }
     }
 
