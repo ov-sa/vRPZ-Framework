40a6d3b530a56231f612a2308d41385eacb1dd6f
Aviril <89867985+Aviril@users.noreply.github.com>
2022-03-14 21:57:03 +0300

    Added `createAssetDummy`

--- [Library]/assetify_library/exports/client.lua
+++ [Library]/assetify_library/exports/client.lua
@@ -55,3 +55,17 @@ function clearBoneAttachment(element, ...)
     if not element or not imports.isElement(element) or not bone.buffer.element[element] then return false end
     return bone.buffer.element[element]:clearElementBuffer(...)
 end
+
+function createAssetDummy(assetType, assetName, objectData)
+    if not assetType or not assetName or not availableAssetPacks[assetType] or not availableAssetPacks[assetType].rwDatas[assetName] then then return false end
+    local cAsset = availableAssetPacks[assetType].rwDatas[assetName].unsyncedData.assetCache[i].cAsset
+    if not cAsset then return false end
+    local cModelInstance = imports.createObject(cAsset.syncedData.modelID, objectData.position.x, objectData.position.y, objectData.position.z, objectData.rotation.x, objectData.rotation.y, objectData.rotation.z)
+    imports.setElementDoubleSided(cModelInstance, true)
+    if cAsset.syncedData.collisionID then
+        local cCollisionInstance = imports.createObject(cAsset.syncedData.collisionID, objectData.position.x, objectData.position.y, objectData.position.z, objectData.rotation.x, objectData.rotation.y, objectData.rotation.z)
+        imports.setElementAlpha(cCollisionInstance, 0)
+        local cStreamer = streamer:create(cModelInstance, "object", {cCollisionInstance})
+    end
+    return cModelInstance
+end
--- [Library]/assetify_library/exports/shared.lua
+++ [Library]/assetify_library/exports/shared.lua
@@ -33,16 +33,3 @@ function setElementAsset(element, ...)
     local arguments = {...}
     return syncer:syncElementModel(element, elementType, arguments[1], arguments[2], arguments[3], arguments[4])
 end
-
-function createAssetObject(assetType, assetName, objectData)
-    if not assetType or not assetName or not availableAssetPacks[assetType] or not availableAssetPacks[assetType].rwDatas[assetName] then then return false end
-    local cAsset = availableAssetPacks[assetType].rwDatas[assetName].unsyncedData.assetCache[i].cAsset
-    local cModelInstance = imports.createObject(cAsset.syncedData.modelID, objectData.position.x, objectData.position.y, objectData.position.z, objectData.rotation.x, objectData.rotation.y, objectData.rotation.z)
-    imports.setElementDoubleSided(cModelInstance, true)
-    if cAsset.syncedData.collisionID then
-        local cCollisionInstance = imports.createObject(cAsset.syncedData.collisionID, objectData.position.x, objectData.position.y, objectData.position.z, objectData.rotation.x, objectData.rotation.y, objectData.rotation.z)
-        imports.setElementAlpha(cCollisionInstance, 0)
-        local cStreamer = streamer:create(cModelInstance, "object", {cCollisionInstance})
-    end
-    return cModelInstance
-end
