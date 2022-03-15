71b76049969572a1f72ac626cb3195830d62b3cc
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-03-15 17:57:31 +0300

    Update server.lua

--- [Handler]/loot_handler/handlers/server.lua
+++ [Handler]/loot_handler/handlers/server.lua
@@ -34,6 +34,19 @@ local buffer = {}
 --[[ Function: Refreshes Ground Loot ]]--
 -----------------------------------------
 
+function refreshLoot(lootMarker, lootItems)
+    if not lootMarker or not lootItems then return false end
+    imports.setElementData(lootMarker, "Inventory:Slots", imports.math.random(availableLoots[lootType].inventorySize[1], availableLoots[lootType].inventorySize[2]))
+    for i = 1, #lootItems, 1 do
+        local j = lootItems[i]
+        imports.setElementData(lootMarker, "Item:"..j.name, imports.math.random(j.amount[1], j.amount[2]))
+        if j.ammo then --TODO: CHEKC IF ITS WEAPON..
+            local ammoName = exports.player_handler:getammoName(j.name) --TODO: CHANE..
+            imports.setElementData(lootMarker, "Item:"..ammoName, imports.math.random(j.ammo[1], j.ammo[2]))
+        end
+    end
+end
+
 function refreshLoots(lootType)
     if not lootType or not availableLoots[lootType] then return false end
     buffer[lootType] = buffer[lootType] or {}
@@ -41,38 +54,18 @@ function refreshLoots(lootType)
         for i, j in imports.pairs(buffer[lootType]) do
             if i and imports.isElement(i) then
                 --TODO: RESET INVENTORY OF THE LOOT..
+                refreshLoot(lootMarker, availableLoots[lootType].lootItems)
             end
         end
     else
         for i = 1, #availableLoots[lootType], 1 do
             local j = availableLoots[lootType][i]
-            local marker = imports.createMarker(j.x, j.y, j.z, "cylinder", availableLoots[lootType].lootSize, 0, 0, 0, 0)
-            imports.setElementData(marker, "Loot:Type", lootType)
-            imports.setElementData(marker, "Loot:Name", availableLoots[lootType].lootName)
-            imports.setElementData(marker, "Loot:Locked", availableLoots[lootType].lootLock)
-            imports.setElementData(marker, "Inventory:Slots", imports.math.random(availableLoots[lootType].inventorySize[1], availableLoots[lootType].inventorySize[2]))
-            imports.setElementData(marker, "Element:Parent", marker)
-            local generatedItems = {}
-            local itemsCount = math.random(1, 2)
-            local tempList = table.copy(availableLoots[lootType].lootItems, true)
-            for i = 0, itemsCount, 1 do
-                if (#tempList) > 0 then
-                    local generatedItemIndex = math.random(#tempList)
-                    local generatedItem = availableLoots[lootType].lootItems[generatedItemIndex]
-                    table.insert(generatedItems, generatedItem)
-                    table.remove(tempList, generatedItemIndex)
-                end
-            end
-            for i, j in ipairs(generatedItems) do
-                imports.setElementData(marker, "Item:"..j.name, j.amountRange)
-                if j.ammoRange then
-                    local weaponAmmoName = exports.player_handler:getWeaponAmmoName(j.name)
-                    if weaponAmmoName then
-                        imports.setElementData(marker, "Item:"..weaponAmmoName, math.random(j.ammoRange[1], j.ammoRange[2]))
-                    end
-                end
-            end
-            buffer[lootType][marker] = true
+            local lootMarker = imports.createMarker(j.x, j.y, j.z, "cylinder", availableLoots[lootType].lootSize, 0, 0, 0, 0)
+            imports.setElementData(lootMarker, "Loot:Type", lootType)
+            imports.setElementData(lootMarker, "Loot:Name", availableLoots[lootType].lootName)
+            imports.setElementData(lootMarker, "Loot:Locked", availableLoots[lootType].lootLock)
+            buffer[lootType][lootMarker] = true
+            refreshLoot(lootMarker, availableLoots[lootType].lootItems)
         end
     end
     return true
