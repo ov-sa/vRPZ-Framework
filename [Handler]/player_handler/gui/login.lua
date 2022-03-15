caf841ca1d1abc3d11896aa6edc25951a586d100
Aviril <89867985+Aviril@users.noreply.github.com>
2022-03-15 12:36:26 +0300

    Update login.lua

--- [Handler]/player_handler/gui/login.lua
+++ [Handler]/player_handler/gui/login.lua
@@ -81,7 +81,7 @@ loginUI = {
                 startX = 5, startY = 5,
                 paddingX = 5, paddingY = 5,
                 size = FRAMEWORK_CONFIGS["UI"]["Login"]["Options"].characters.options.size, iconSize = FRAMEWORK_CONFIGS["UI"]["Login"]["Options"].characters.options.iconSize,
-                tooltipFont = FRAMEWORK_FONTS[8],
+                tooltipFont = FRAMEWORK_FONTS[8], tooltipFontColor = -1, --TODO: CONFIGURE FONT AND FONT COLOR
                 iconColor = FRAMEWORK_CONFIGS["UI"]["Login"]["Options"].characters.options.iconColor,
                 bgColor = imports.tocolor(imports.unpackColor(FRAMEWORK_CONFIGS["UI"]["Login"]["Options"].characters.options.bgColor)),
                 {iconTexture = imports.beautify.assets["images"]["arrow/left.rw"], tooltip = {identifier = FRAMEWORK_CONFIGS["UI"]["Login"]["Options"].characters.options.tooltips.previous}, exec = function() loginUI.phases[2].manageCharacter("previous") end},
@@ -588,7 +588,7 @@ loginUI.renderUI = function(renderData)
                 imports.beautify.native.drawImage(loginUI.phases[2].options.iconX, j.iconY, loginUI.phases[2].options.iconSize, loginUI.phases[2].options.iconSize, j.iconTexture, 0, 0, 0, imports.tocolor(loginUI.phases[2].options.iconColor[1], loginUI.phases[2].options.iconColor[2], loginUI.phases[2].options.iconColor[3], loginUI.phases[2].options.iconColor[4]*j.animAlphaPercent), false)
                 if (j.tooltip.animPercent > 0) then
                     imports.beautify.native.drawRectangle(loginUI.phases[2].options.startX + loginUI.phases[2].options.size + 5, j.startY, j.tooltip.animPercent*j.tooltip.width, loginUI.phases[2].options.size, loginUI.phases[2].options.bgColor, false)
-                    imports.beautify.native.drawText(j.tooltip.identifier[FRAMEWORK_LANGUAGE], loginUI.phases[2].options.startX + loginUI.phases[2].options.size + 5, j.startY, loginUI.phases[2].options.startX + loginUI.phases[2].options.size + 5 + j.tooltip.animPercent, j.startY + loginUI.phases[2].options.size, -1, 1, loginUI.phases[2].options.tooltipFont, "center", "center", true)
+                    imports.beautify.native.drawText(j.tooltip.identifier[FRAMEWORK_LANGUAGE], loginUI.phases[2].options.startX + loginUI.phases[2].options.size + 5, j.startY, loginUI.phases[2].options.startX + loginUI.phases[2].options.size + 5 + j.tooltip.animPercent, j.startY + loginUI.phases[2].options.size, loginUI.phases[2].options.tooltipFontColor, 1, loginUI.phases[2].options.tooltipFont, "center", "center", true)
                 end
             end
         elseif loginUI.phase == 3 then
