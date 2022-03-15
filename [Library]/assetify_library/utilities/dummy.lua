5e6270a0332d34cb7139a92f6bf324ecd541e7c4
Aviril <89867985+Aviril@users.noreply.github.com>
2022-03-15 13:45:09 +0300

    Update dummy.lua

--- [Library]/assetify_library/utilities/dummy.lua
+++ [Library]/assetify_library/utilities/dummy.lua
@@ -21,9 +21,9 @@ local imports = {
 }
 
 
----------------------
---[[ Class: Bone ]]--
----------------------
+----------------------
+--[[ Class: Dummy ]]--
+----------------------
 
 dummy = {
     buffer = {}
@@ -31,12 +31,12 @@ dummy = {
 dummy.__index = dummy
 
 function dummy:create(...)
-    local cObject = imports.setmetatable({}, {__index = self})
-    if not cObject:load(...) then
-        cObject = nil
+    local cDummy = imports.setmetatable({}, {__index = self})
+    if not cDummy:load(...) then
+        cDummy = nil
         return false
     end
-    return cObject
+    return cDummy
 end
 
 function dummy:destroy(...)
@@ -52,17 +52,13 @@ function dummy:clearElementBuffer(element)
     return true
 end
 
-function dummy:load(element, parent, boneData)
+function dummy:load(dummyData)
     if not self or (self == dummy) then return false end
-    if not element or not imports.isElement(element) or not parent or not imports.isElement(parent) or not boneData or (element == parent) or dummy.buffer[element] then return false end
+    if not dummyData then return false end
     self.element = element
-    self.parent = parent
-    if not self:refresh(boneData) then return false end
     imports.setElementCollisionsEnabled(element, false)
     self.cStreamer = streamer:create(element, "dummy", {parent})
     dummy.buffer[element] = self
-    dummy.buffer.parent[parent] = dummy.buffer.parent[parent] or {}
-    dummy.buffer.parent[parent][self] = true
     return true
 end
 
