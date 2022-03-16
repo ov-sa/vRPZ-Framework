c28150b29ecae65d5eee0c66d81a3258bbf53d5d
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-03-16 21:29:18 +0300

    Update chatbox.lua

--- [Handler]/player_handler/handlers/hud/chatbox.lua
+++ [Handler]/player_handler/handlers/hud/chatbox.lua
@@ -4,7 +4,7 @@
      Author: vStudio
      Developer(s): Mario, Tron, Aviril
      DOC: 31/01/2022
-     Desc: Chatbox HUD Handler ]]--
+     Desc: Channel Handler ]]--
 ----------------------------------------------------------------
 
 
@@ -25,40 +25,38 @@ local imports = {
 
 
 -------------------
---[[ Variables ]]--
+--[[ Utilities ]]--
 -------------------
 
 imports.addEventHandler("onPlayerChat", root, function(message, messageType)
     imports.cancelEvent()
     if not CPlayer.isInitialized(source) or not isValidMessage(message) or (messageType ~= 0) then return false end
-    if not isPlayerAllowedToChat(source) return false end
+    local channelIndex = CPlayer.getChannel(source)
+    if not channelIndex then return false end
+    local channelData = FRAMEWORK_CONFIGS["Game"]["Chatbox"]["Chats"][channelIndex]
+    if not channelData then return false end
     if imports.isPlayerMuted(source) then
         imports.outputChatBox("- You are muted!", source, 255, 0, 0)
         return false
     end
 
-    local channel = CPlayer.CChannel[source]
-    local channelColor = "#FFC800"
     local playerName = imports.getPlayerName(source)
-	local r, g, b = imports.getPlayerNametagColor(source)
-    if channel == 1 then
-        local playerList = imports.getElementsByType("player")
+	local playerTagColor = RGBToHex(imports.getPlayerNametagColor(source))
+    local playerList = false
+    if channelIndex == 1 then
+        playerList = imports.getElementsByType("player")
+    elseif channelIndex == 2 then
+        playerList = imports.getPlayersWithinRange(source, FRAMEWORK_CONFIGS["Game"]["Chatbox"]["Proximity_Range"])
+    end
+    if playerList then
         for i = 1, #playerList, 1 do
             local j = playerList[i]
             if CPlayer.isInitialized(j) then
-                imports.outputChatBox("#FFFFFF["..channelColor..channel.."#FFFFFF] ˧ "..RGBToHex(r, g, b)..playerName..": "..RGBToHex(255, 149, 79)..message, j, 255, 255, 255, true)
-            end
-        end
-    elseif channel == 2 then
-        local playerList = imports.getPlayersWithinRange(source, FRAMEWORK_CONFIGS["Game"]["Chatbox"]["Proximity_Range"])
-        for i = 1, #playerList, 1 do
-            local j = playerList[i]
-            if CPlayer.isInitialized(j) then
-                imports.outputChatBox("#FFFFFF["..channelColor..chatType.."#FFFFFF] ˧ "..RGBToHex(r, g, b)..playerName..": "..RGBToHex(149, 255, 149)..message, j, 255, 255, 255, true)
+                imports.outputChatBox("#FFFFFF["..(channelData.tagColor)..(channelData.name).."#FFFFFF] ˧ "..playerTagColor..playerName..": "..(channelData.messageColor)..message, j, 255, 255, 255, true)
             end
         end
     end
-    imports.outputServerLog("["..channel.."] "..playerName..": "..message)
+    imports.outputServerLog("["..(channelData.name).."] "..playerName..": "..message)
 end)
 
 imports.bindKey(FRAMEWORK_CONFIGS["Game"]["Chatbox"]["Channel_ShuffleKey"], "down", function(player)
