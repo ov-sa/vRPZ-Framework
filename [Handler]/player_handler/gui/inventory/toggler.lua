1a30975c5372b8d221e76b2cafa55f51af0670db
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-03-20 13:32:12 +0300

    Update toggler.lua

--- [Handler]/player_handler/gui/inventory/toggler.lua
+++ [Handler]/player_handler/gui/inventory/toggler.lua
@@ -1,6 +1,5 @@
 --[[
 function inventoryUI.toggleUI(true)    
-    createItemBox(inventoryUI.gui.equipment.startX + 250 + 50, inventoryUI.gui.equipment.startY + 25, 1, localPlayer, "Inventory")
     inventoryUI.vicinity = isPlayerInLoot(localPlayer)
     if inventoryUI.vicinity and isElement(inventoryUI.vicinity) then
         local vicinityLockedStatus = inventoryUI.vicinity:getData("Loot:Locked")
