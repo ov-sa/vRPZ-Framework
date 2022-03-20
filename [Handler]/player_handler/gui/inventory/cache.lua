2b554e1b5fcd976bf2d7fd476ab81460826fa150
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-03-20 13:27:41 +0300

    Update cache.lua

--- [Handler]/player_handler/gui/inventory/cache.lua
+++ [Handler]/player_handler/gui/inventory/cache.lua
@@ -309,6 +309,7 @@ inventoryUI.toggleUI = function(state)
         end
         inventoryUI.destroyBuffer(localPlayer)
         inventoryUI.destroyBuffer(inventoryUI.vicinityInventory.element)
+        inventoryUI.vicinityInventory.element = nil
         inventoryUI.opacityAdjuster.element = nil
     end
     inventoryUI.detachItem(true)
