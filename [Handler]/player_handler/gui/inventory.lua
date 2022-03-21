b6236aa611af8cb4929c885defd159fc920fd969
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-03-21 19:06:34 +0300

    Update inventory.lua

--- [Handler]/player_handler/gui/inventory.lua
+++ [Handler]/player_handler/gui/inventory.lua
@@ -353,7 +353,7 @@ inventoryUI.renderUI = function(renderData)
                 if isLMBClicked then
                     if not inventoryUI.attachedItem then
                         local cursorX, cursorY = imports.getAbsoluteCursorPosition()
-                        local slot_prevX, slot_prevY = vicinity_bufferCache[vicinity_isSlotHovered].startX, vicinity_bufferCache[vicinity_isSlotHovered].startY
+                        local slot_prevX, slot_prevY = vicinity_startX + inventoryUI.margin + vicinity_bufferCache[vicinity_isSlotHovered].startX, vicinity_startY + inventoryUI.margin + vicinity_bufferCache[vicinity_isSlotHovered].startY
                         inventoryUI.attachItem(inventoryUI.vicinityInventory.element, vicinity_bufferCache[vicinity_isSlotHovered].item, vicinity_isSlotHovered, slot_prevX, slot_prevY, vicinity_bufferCache[vicinity_isSlotHovered].width, vicinity_bufferCache[vicinity_isSlotHovered].height, cursorX - slot_prevX, cursorY - slot_prevY)
                     end
                 end
