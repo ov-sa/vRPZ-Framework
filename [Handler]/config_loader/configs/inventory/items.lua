36c9c08143a83df0513f0aefde5f3a20ed792f60
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-03-21 13:13:13 +0300

    Update items.lua

--- [Handler]/config_loader/configs/inventory/items.lua
+++ [Handler]/config_loader/configs/inventory/items.lua
@@ -15,10 +15,10 @@
 configVars["Inventory"]["Items"] = {
 
     ["Primary"] = {
-        ["awm"] = {name = "AWM", weight = {rows = 2, columns = 8}, itemObjectID = 2827, itemPickupDetails = {scale = 1, rotX = 90}, weapon = {id = 34, ammo = "sniper_ammo", mag = 5, damage = 13000, fireDelay = 1500}},
-        ["colt_model_733"] = {name = "Colt Model 733", weight = {rows = 2, columns = 8}, itemObjectID = 2827, itemPickupDetails = {scale = 1, rotX = 90}, weapon = {id = 34, ammo = "sniper_ammo", mag = 5, damage = 13000, fireDelay = 1500}},
-        ["double_barreled_shotgun"] = {name = "Double Barreled Shotgun", weight = {rows = 2, columns = 8}, itemObjectID = 2827, itemPickupDetails = {scale = 1, rotX = 90}, weapon = {id = 34, ammo = "sniper_ammo", mag = 5, damage = 13000, fireDelay = 1500}},
-        ["fn_fal_g"] = {name = "FN FAL G", weight = {rows = 2, columns = 8}, itemObjectID = 2827, itemPickupDetails = {scale = 1, rotX = 90}, weapon = {id = 34, ammo = "sniper_ammo", mag = 5, damage = 13000, fireDelay = 1500}},
+        ["awm"] = {name = "AWM", weight = {rows = 2, columns = 6}, itemObjectID = 2827, itemPickupDetails = {scale = 1, rotX = 90}, weapon = {id = 34, ammo = "sniper_ammo", mag = 5, damage = 13000, fireDelay = 1500}},
+        ["colt_model_733"] = {name = "Colt Model 733", weight = {rows = 2, columns = 6}, itemObjectID = 2827, itemPickupDetails = {scale = 1, rotX = 90}, weapon = {id = 34, ammo = "sniper_ammo", mag = 5, damage = 13000, fireDelay = 1500}},
+        ["double_barreled_shotgun"] = {name = "Double Barreled Shotgun", weight = {rows = 2, columns = 6}, itemObjectID = 2827, itemPickupDetails = {scale = 1, rotX = 90}, weapon = {id = 34, ammo = "sniper_ammo", mag = 5, damage = 13000, fireDelay = 1500}},
+        ["fn_fal_g"] = {name = "FN FAL G", weight = {rows = 2, columns = 6}, itemObjectID = 2827, itemPickupDetails = {scale = 1, rotX = 90}, weapon = {id = 34, ammo = "sniper_ammo", mag = 5, damage = 13000, fireDelay = 1500}},
     },
 
     ["Secondary"] = {},
