a1824bd81bd0e088185530cd6b56a63e24ae5133
Aviril <89867985+Aviril@users.noreply.github.com>
2022-03-27 16:34:40 +0300

    Init UI template

--- [Handler]/config_loader/configs/templates/index.lua
+++ [Handler]/config_loader/configs/templates/index.lua
@@ -25,14 +25,6 @@ configVars["Templates"] = {
             color = {100, 100, 100, 255},
             fontColor = {200, 200, 200, 255},
             hoverColor = {200, 200, 200, 255}
-        },
-    
-        ["beautify_selector"] = {
-            fontPaddingY = 0,
-            font = {"files/assets/fonts/teko_medium.rw", 16, "beautify_library"},
-            color = {100, 100, 100, 255},
-            fontColor = {200, 200, 200, 255},
-            hoverColor = {200, 200, 200, 255}
         }
     }
 
---
+++ [Library]/assetify_mapper/settings/client.lua
@@ -0,0 +1,19 @@
+----------------------------------------------------------------
+--[[ Resource: Assetify Mapper
+     Script: settings: client.lua
+     Author: vStudio
+     Developer(s): Tron
+     DOC: 25/03/2022
+     Desc: Client Sided Settings ]]--
+----------------------------------------------------------------
+
+
+------------------
+--[[ Settings ]]--
+------------------
+
+availableTemplates = {
+    ["beautify_card"] = {
+        color = {0, 0, 0, 0}
+    }
+}
