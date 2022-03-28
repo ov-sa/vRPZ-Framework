642fb8f821fabb9f149a72c297b7fa694ed5efbe
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-03-28 21:02:09 +0300

    Update client.lua

--- [Library]/assetify_mapper/gui/client.lua
+++ [Library]/assetify_mapper/gui/client.lua
@@ -121,6 +121,7 @@ end
 -------------------------------------------
 
 mapper.ui.renderToolWnd = function()
+    imports.beautify.gridlist.setSelection(mapper.ui.sceneWnd.propLst.element, (mapper.isTargettingDummy and mapper.buffer.element[(mapper.isTargettingDummy)].id) or 0)
     imports.beautify.native.drawText("Selected Prop: "..(mapper.isTargettingDummy and imports.beautify.gridlist.getRowData(mapper.ui.sceneWnd.propLst.element, mapper.buffer.element[(mapper.isTargettingDummy)].id, 1) or "-").."\nTranslator Mode: "..((mapper.translationMode and mapper.translationMode.type) or "-").."\nTranslation Axis: "..((mapper.translationMode and mapper.translationMode.axis) or "-"), 0, mapper.ui.margin, CLIENT_MTA_RESOLUTION[1] - mapper.ui.margin, CLIENT_MTA_RESOLUTION[2], mapper.ui.toolWnd.fontColor, 1, mapper.ui.toolWnd.font, "right", "top", true, true, false)
 end
 
