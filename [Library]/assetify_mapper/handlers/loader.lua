6ec55c16ca3a5cfd83a5efb9acbb5b350878274f
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-03-31 13:34:16 +0300

    Update loader.lua

--- [Library]/assetify_mapper/handlers/loader.lua
+++ [Library]/assetify_mapper/handlers/loader.lua
@@ -71,14 +71,14 @@ imports.addEventHandler("onClientResourceStart", resource, function()
     end
     if imports.assetify.isLoaded() then
         Assetify_Props = imports.assetify.getAssets(mapper.assetPack) or {}
-        mapper:enable(true)
         mapper:toggle(true)
+        mapper:enable(true)
     else
         local booterWrapper = nil
         booterWrapper = function()
             Assetify_Props = imports.assetify.getAssets(mapper.assetPack) or {}
-            mapper:enable(true)
             mapper:toggle(true)
+            mapper:enable(true)
             imports.removeEventHandler("onAssetifyLoad", root, booterWrapper)
         end
         imports.addEventHandler("onAssetifyLoad", root, booterWrapper)
