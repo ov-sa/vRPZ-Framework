a9a6f8493ff3af3a8d303cf0ea90ae424a7d9cab
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-04-09 17:45:35 +0300

    Добавление поддержки русского языка

--- [Handler]/config_loader/configs/gameplay/gui/loading.lua
+++ [Handler]/config_loader/configs/gameplay/gui/loading.lua
@@ -29,43 +29,53 @@ configVars["UI"]["Loading"] = {
         fontColor = {200, 200, 200, 255},
         {
             ["EN"] = "Upgrade your skills to unlock higher blood limit!",
-            ["TR"] = "Daha yüksek kan limitinin kilidini açmak için yeteneklerinizi geliştirmelisin!"
+            ["TR"] = "Daha yüksek kan limitinin kilidini açmak için yeteneklerinizi geliştirmelisin!",
+            ["RU"] = "Обновите свои навыки, чтобы разблокировать более высокий лимит крови!"
         },
         {
             ["EN"] = "Use Antiradiation suits to evade radiations effectively!",
-            ["TR"] = "Radyasyonlardan etkili bir şekilde kaçmak için Radyasyon Önleyici giysiler kullanmalısın!"
+            ["TR"] = "Radyasyonlardan etkili bir şekilde kaçmak için Radyasyon Önleyici giysiler kullanmalısın!",
+            ["RU"] = "Используйте антирадиационные костюмы, чтобы эффективно уклоняться от радиации!"
         },
         {
             ["EN"] = "Use helmets to evade headshots!",
-            ["TR"] = "Kafa vuruşlarından kaçınmak için kask kullanmalısın!"
+            ["TR"] = "Kafa vuruşlarından kaçınmak için kask kullanmalısın!",
+            ["RU"] = "Используйте шлемы, чтобы уклоняться от выстрелов в голову!"
         },
         {
             ["EN"] = "Use armors to evade damages!",
-            ["TR"] = "Hasarlardan kaçınmak için zırhları kullanmalısın!"
+            ["TR"] = "Hasarlardan kaçınmak için zırhları kullanmalısın!",
+            ["RU"] = "Используйте доспехи, чтобы избежать повреждений!"
         },
         {
             ["EN"] = "Use backpacks to carry more items!",
-            ["TR"] = "Daha fazla eşya taşımak için sırt çantalarını kullanmalısın!"
+            ["TR"] = "Daha fazla eşya taşımak için sırt çantalarını kullanmalısın!",
+            ["RU"] = "Используйте рюкзаки, чтобы нести больше предметов!"
         },
         {
             ["EN"] = "Always be alarmed, never trust anyone!",
-            ["TR"] = "Her zaman dikkatli olmalısın, asla kimseye güvenmemelisin!"
+            ["TR"] = "Her zaman dikkatli olmalısın, asla kimseye güvenmemelisin!",
+            ["RU"] = "Будь всегда начеку, никогда никому не верь!"
         },
         {
             ["EN"] = "Revive your group members to gain more reputation!",
-            ["TR"] = "Daha fazla itibar kazanmak için grup üyelerinizi canlandırın!"
+            ["TR"] = "Daha fazla itibar kazanmak için grup üyelerinizi canlandırın!",
+            ["RU"] = "Оживите членов вашей группы, чтобы получить больше репутации!"
         },
         {
             ["EN"] = "Killing your group members will reduce your reputation!",
-            ["TR"] = "Grup üyelerinizi öldürmek itibarınızı düşürür!"
+            ["TR"] = "Grup üyelerinizi öldürmek itibarınızı düşürür!",
+            ["RU"] = "Убийство членов вашей группы снизит вашу репутацию!"
         },
         {
             ["EN"] = "Maintain higher reputation to unlock high tier loots!",
-            ["TR"] = "Yüksek seviyeli ganimetlerin kilidini açmak için daha yüksek itibarı koruyun!"
+            ["TR"] = "Yüksek seviyeli ganimetlerin kilidini açmak için daha yüksek itibarı koruyun!",
+            ["RU"] = "Поддерживайте более высокую репутацию, чтобы разблокировать добычу высокого уровня!"
         },
         {
             ["EN"] = "Collect GPS & Compass to help you travel on the map!",
-            ["TR"] = "Haritada seyahat etmenize yardımcı olması için GPS ve Pusula toplayın!"
+            ["TR"] = "Haritada seyahat etmenize yardımcı olması için GPS ve Pusula toplayın!",
+            ["RU"] = "Собирайте GPS и компас, чтобы путешествовать по карте!"
         }
     }
 
