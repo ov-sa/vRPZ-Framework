a286c1a816e9fadaf4c67c260530a4dd705d24d2
Aviril <89867985+Aviril@users.noreply.github.com>
2022-04-11 10:26:08 +0300

    Update index.lua

--- [Handler]/player_handler/modules/character/shared/index.lua
+++ [Handler]/player_handler/modules/character/shared/index.lua
@@ -54,19 +54,30 @@ CCharacter = {
 
     generateClothing = function(characterIdentity)
         if not characterIdentity then return false end
-        local clothingData = {
+        local clothingData, clothingTextures = {
             gender = FRAMEWORK_CONFIGS["Character"]["Identity"]["Gender"][(characterIdentity.gender)],
             hair = FRAMEWORK_CONFIGS["Character"]["Clothing"]["Facial"]["Hair"][(characterIdentity.gender)][(characterIdentity.hair)],
             face = FRAMEWORK_CONFIGS["Character"]["Clothing"]["Facial"]["Face"][(characterIdentity.gender)][(characterIdentity.face)],
             upper = FRAMEWORK_CONFIGS["Character"]["Clothing"]["Upper"][(characterIdentity.gender)][(characterIdentity.upper)],
             lower = FRAMEWORK_CONFIGS["Character"]["Clothing"]["Lower"][(characterIdentity.gender)][(characterIdentity.lower)],
             shoes = FRAMEWORK_CONFIGS["Character"]["Clothing"]["Shoes"][(characterIdentity.gender)][(characterIdentity.shoes)]
-        }
-        return clothingData.gender.assetName, (clothingData.hair.clumpName or "")..(clothingData.face.clumpName or "")..(clothingData.upper.clumpName or "")..(clothingData.lower.clumpName or "")..(clothingData.shoes.clumpName or ""), {
-            [(clothingData.upper.clumpTexture[1])] = clothingData.upper.clumpTexture[2],
-            [(clothingData.lower.clumpTexture[1])] = clothingData.lower.clumpTexture[2],
-            [(clothingData.shoes.clumpTexture[1])] = clothingData.shoes.clumpTexture[2]
-        }
+        }, {}
+        if clothingData.hair.clumpTexture then
+            clothingTextures[(clothingData.hair.clumpTexture[1])] = clothingData.hair.clumpTexture[2]
+        end
+        if clothingData.face.clumpTexture then
+            clothingTextures[(clothingData.face.clumpTexture[1])] = clothingData.face.clumpTexture[2]
+        end
+        if clothingData.upper.clumpTexture then
+            clothingTextures[(clothingData.upper.clumpTexture[1])] = clothingData.upper.clumpTexture[2]
+        end
+        if clothingData.lower.clumpTexture then
+            clothingTextures[(clothingData.lower.clumpTexture[1])] = clothingData.lower.clumpTexture[2]
+        end
+        if clothingData.shoes.clumpTexture then
+            clothingTextures[(clothingData.shoes.clumpTexture[1])] = clothingData.shoes.clumpTexture[2]
+        end
+        return clothingData.gender.assetName, (clothingData.hair.clumpName or "")..(clothingData.face.clumpName or "")..(clothingData.upper.clumpName or "")..(clothingData.lower.clumpName or "")..(clothingData.shoes.clumpName or ""), clothingTextures
     end,
 
     setHealth = function(player, amount)
