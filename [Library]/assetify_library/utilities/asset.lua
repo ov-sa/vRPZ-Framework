7e8b7dbf24678e5cf96fec0e8de4bab6d2d618db
Aviril <89867985+Aviril@users.noreply.github.com>
2022-04-15 15:12:41 +0300

    Update asset.lua

--- [Library]/assetify_library/utilities/asset.lua
+++ [Library]/assetify_library/utilities/asset.lua
@@ -90,9 +90,9 @@ if localPlayer then
         if not self or (self == asset) then return false end
         if not assetType or not assetName or not assetPack or not assetPack.assetType or not rwCache or not assetManifest or not assetData or not rwPaths then return false end
         local loadState = false
-        if assetPack == "animation" then
+        if assetType == "animation" then
             if rwPaths.ifp then
-                rwCache.ifp[(rwPaths.ifp)] = imports.engineLoadIFP((assetManifest.encryptKey and imports.decodeString("tea", imports.file.read(rwPaths.ifp), {key = assetManifest.encryptKey})) or rwPaths.ifp, assetPack.."."..assetName)
+                rwCache.ifp[(rwPaths.ifp)] = imports.engineLoadIFP((assetManifest.encryptKey and imports.decodeString("tea", imports.file.read(rwPaths.ifp), {key = assetManifest.encryptKey})) or rwPaths.ifp, assetType.."."..assetName)
                 if rwCache.ifp[(rwPaths.ifp)] then
                     assetData.cAsset = self
                     self.rwPaths = rwPaths
