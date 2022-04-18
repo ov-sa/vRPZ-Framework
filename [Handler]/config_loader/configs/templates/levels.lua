312673a480ab81af3e1b87f750aebcaaa46a4095
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-04-18 20:18:39 +0300

    Initialized level template

---
+++ [Handler]/config_loader/configs/templates/levels.lua
@@ -0,0 +1,17 @@
+----------------------------------------------------------------
+--[[ Resource: Config Loader
+     Script: configs: templates: levels.lua
+     Author: vStudio
+     Developer(s): Mario, Tron, Aviril
+     DOC: 31/01/2022
+     Desc: Level Templates Configns ]]--
+----------------------------------------------------------------
+
+
+------------------
+--[[ Configns ]]--
+------------------
+
+configVars["Templates"]["Levels"] = {
+    
+}
