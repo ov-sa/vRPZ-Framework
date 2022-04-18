4e88c175380537698875cee690651edd1f2f3cd6
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-04-18 20:28:14 +0300

    No need of range based, making it linearly

--- [Handler]/config_loader/configs/templates/levels.lua
+++ [Handler]/config_loader/configs/templates/levels.lua
@@ -14,24 +14,25 @@
 
 configVars["Templates"]["Levels"] = {
     
-    baseEXP = 1000, EXPMultiplier = 1.2, MaxLevel = 200,
+    baseEXP = 1000, expMultiplier = 1.2,
 
-    ranks = {
-        {name = "Rank 1", range = {1, 5}},
-        {name = "Rank 2", range = {1, 5}},
-        {name = "Rank 3", range = {1, 5}},
-        {name = "Rank 4", range = {1, 5}},
-        {name = "Rank 5", range = {1, 5}},
-        {name = "Rank 6", range = {1, 5}},
-        {name = "Rank 7", range = {1, 5}},
-        {name = "Rank 8", range = {1, 5}},
-        {name = "Rank 9", range = {1, 5}},
-        {name = "Rank 10", range = {1, 5}},
-        {name = "Rank 11", range = {1, 5}},
-        {name = "Rank 12", range = {1, 5}},
-        {name = "Rank 13", range = {1, 5}},
-        {name = "Rank 14", range = {1, 5}},
-        {name = "Rank 15", range = {1, 5}}
+    ["Max_Level"] = 200,
+    ["Ranks"] = {
+        {name = "Rank 1"},
+        {name = "Rank 2"},
+        {name = "Rank 3"},
+        {name = "Rank 4"},
+        {name = "Rank 5"},
+        {name = "Rank 6"},
+        {name = "Rank 7"},
+        {name = "Rank 8"},
+        {name = "Rank 9"},
+        {name = "Rank 10"},
+        {name = "Rank 11"},
+        {name = "Rank 12"},
+        {name = "Rank 13"},
+        {name = "Rank 14"},
+        {name = "Rank 15"}
     }
 
 }
