3465a013517eb439f10e43b607fc1b7e47bf43bb
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-04-18 20:24:07 +0300

    Update levels.lua

--- [Handler]/config_loader/configs/templates/levels.lua
+++ [Handler]/config_loader/configs/templates/levels.lua
@@ -17,20 +17,21 @@ configVars["Templates"]["Levels"] = {
     baseEXP = 1000, EXPMultiplier = 1.2, MaxLevel = 200,
 
     ranks = {
-        {name = "RankNameHere", range = {1, 5}},
-        {name = "RankNameHere", range = {1, 5}},
-        {name = "RankNameHere", range = {1, 5}},
-        {name = "RankNameHere", range = {1, 5}},
-        {name = "RankNameHere", range = {1, 5}},
-        {name = "RankNameHere", range = {1, 5}},
-        {name = "RankNameHere", range = {1, 5}},
-        {name = "RankNameHere", range = {1, 5}},
-        {name = "RankNameHere", range = {1, 5}},
-        {name = "RankNameHere", range = {1, 5}},
-        {name = "RankNameHere", range = {1, 5}},
-        {name = "RankNameHere", range = {1, 5}},
-        {name = "RankNameHere", range = {1, 5}},
-        {name = "RankNameHere", range = {1, 5}}
+        {name = "Rank 1", range = {1, 5}},
+        {name = "Rank 2", range = {1, 5}},
+        {name = "Rank 3", range = {1, 5}},
+        {name = "Rank 4", range = {1, 5}},
+        {name = "Rank 5", range = {1, 5}},
+        {name = "Rank 6", range = {1, 5}},
+        {name = "Rank 7", range = {1, 5}},
+        {name = "Rank 8", range = {1, 5}},
+        {name = "Rank 9", range = {1, 5}},
+        {name = "Rank 10", range = {1, 5}},
+        {name = "Rank 11", range = {1, 5}},
+        {name = "Rank 12", range = {1, 5}},
+        {name = "Rank 13", range = {1, 5}},
+        {name = "Rank 14", range = {1, 5}},
+        {name = "Rank 15", range = {1, 5}}
     }
 
 }
