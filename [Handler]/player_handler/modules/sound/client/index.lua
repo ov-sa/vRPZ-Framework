75b387fdc5d0347f2dae0ef67a814bd20ee91d25
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-04-19 12:55:23 +0300

    Update index.lua

--- [Handler]/player_handler/modules/sound/client/index.lua
+++ [Handler]/player_handler/modules/sound/client/index.lua
@@ -13,6 +13,7 @@
 -----------------
 
 local imports = {
+    type = type,
     tonumber = tonumber,
     setTimer = setTimer,
     getSoundLength = getSoundLength,
@@ -25,15 +26,21 @@ local imports = {
 ----------------
 
 CSound = {
-    CBuffer = {}
+    CBuffer = {},
+    CAmbience = {}
 }
 
 CSound.playAmbience = function(ambienceType, index, skipLoop)
     if not FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType] then return false end
-    if not index then
-        index = imports.math.random(CATEGORYTABLEHERE)
+    local ambienceCategory = false
+    if not index or (imports.type(FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].category) == "table") then
+        CSound.CBuffer[ambienceType] = CSound.CBuffer[ambienceType] or {}
+        CSound.CBuffer[ambienceType].categoryIndex = (CSound.CBuffer[ambienceType].categoryIndex or 0) + 1
+        CSound.CBuffer[ambienceType].categoryIndex = (FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].category[(CSound.CBuffer[ambienceType].categoryIndex)] and CSound.CBuffer[ambienceType].categoryIndex) or 1
+        ambienceCategory = FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].category[(CSound.CBuffer[ambienceType].categoryIndex)]
+        index = imports.math.random(#CSound.CAmbience[ambienceCategory])
     end
-    local cSound = CGame.playSound(FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].assetName, ambienceType, index)
+    local cSound = CGame.playSound(FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].assetName, ambienceCategory, index)
     if not cSound then return false end
     local soundDuration = imports.getSoundLength(cSound) or 0
     if not skipLoop and FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].loopInterval then
