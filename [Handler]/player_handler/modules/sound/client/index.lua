1a8f458f25950bfa81b8d6acfb26d73e029a5cdb
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-04-19 13:00:05 +0300

    Update index.lua

--- [Handler]/player_handler/modules/sound/client/index.lua
+++ [Handler]/player_handler/modules/sound/client/index.lua
@@ -32,21 +32,24 @@ CSound = {
 
 CSound.playAmbience = function(ambienceType, index, skipLoop)
     if not FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType] then return false end
+    CSound.CBuffer[ambienceType] = CSound.CBuffer[ambienceType] or {}
     local ambienceCategory = false
     if not index or (imports.type(FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].category) == "table") then
-        CSound.CBuffer[ambienceType] = CSound.CBuffer[ambienceType] or {}
         CSound.CBuffer[ambienceType].categoryIndex = (CSound.CBuffer[ambienceType].categoryIndex or 0) + 1
         CSound.CBuffer[ambienceType].categoryIndex = (FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].category[(CSound.CBuffer[ambienceType].categoryIndex)] and CSound.CBuffer[ambienceType].categoryIndex) or 1
         ambienceCategory = FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].category[(CSound.CBuffer[ambienceType].categoryIndex)]
         index = imports.math.random(#CSound.CAmbience[ambienceCategory])
+    else
+        ambienceCategory = FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].category
     end
     local cSound = CGame.playSound(FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].assetName, ambienceCategory, index)
     if not cSound then return false end
     local soundDuration = imports.getSoundLength(cSound) or 0
-    if not skipLoop and FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].loopInterval then
-        imports.setTimer(function()
+    if not skipLoop and FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].loopInterval and not CSound.CBuffer[ambienceType].intervalTimer then
+        CSound.CBuffer[ambienceType].intervalTimer = imports.setTimer(function(ambienceType)
+            CSound.CBuffer[ambienceType].intervalTimer = nil
             CSound.playAmbience(ambienceType)
-        end, (soundDuration*1000) + FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].loopInterval, 1)
+        end, (soundDuration*1000) + FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].loopInterval, 1, ambienceType)
     end
     return true 
 end
