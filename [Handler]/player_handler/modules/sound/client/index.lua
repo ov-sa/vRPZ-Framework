e15977f0ed4c46ec8208dafed28e238307ce9d38
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-04-19 12:41:03 +0300

    Integrated w/ framework configs

--- [Handler]/player_handler/modules/sound/client/index.lua
+++ [Handler]/player_handler/modules/sound/client/index.lua
@@ -29,19 +29,19 @@ CSound = {
 }
 
 CSound.playAmbience = function(ambienceType, index)
-    if not configVars["Templates"]["Ambiences"][ambienceType] then return false end
+    if not FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType] then return false end
     if not index then
         index = imports.math.random(CATEGORYTABLEHERE)
     end
-    local cSound = CGame.playSound(configVars["Templates"]["Ambiences"][ambienceType].assetName, ambienceType, index)
+    local cSound = CGame.playSound(FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].assetName, ambienceType, index)
     if not cSound then return false end
     local soundDuration = imports.getSoundLength(cSound) or 0
-    if configVars["Templates"]["Ambiences"][ambienceType].loopInterval then
+    if FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].loopInterval then
         imports.setTimer(function()
-            CSound.playAmbience(ambienceType, _, configVars["Templates"]["Ambiences"][ambienceType].loopInterval)
-        end, (soundDuration*1000) + configVars["Templates"]["Ambiences"][ambienceType].loopInterval, 1)
+            CSound.playAmbience(ambienceType, _, FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].loopInterval)
+        end, (soundDuration*1000) + FRAMEWORK_CONFIGS["Templates"]["Ambiences"][ambienceType].loopInterval, 1)
     end
     return true 
 end
 
-CSound.playAmbience(configVars["Templates"]["Ambiences"]["Short_Ambience"].assetName, _, configVars["Templates"]["Ambiences"]["Short_Ambience"].loopInterval)
+CSound.playAmbience(FRAMEWORK_CONFIGS["Templates"]["Ambiences"]["Short_Ambience"].assetName, _, FRAMEWORK_CONFIGS["Templates"]["Ambiences"]["Short_Ambience"].loopInterval)
