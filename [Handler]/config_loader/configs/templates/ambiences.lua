0d5c135ad1d17e6509d9b1ca7f7a41884f9d1682
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-04-30 14:08:15 +0300

    Немного уменьшен объем окружающего звука

--- [Handler]/config_loader/configs/templates/ambiences.lua
+++ [Handler]/config_loader/configs/templates/ambiences.lua
@@ -17,13 +17,13 @@ configVars["Templates"]["Ambiences"] = {
     ["Short"] = {
         assetName = "vRPZ_Ambience",
         category = "short",
-        volume = 1,
         loopInterval = 20000
     },
 
     ["Long"] = {
         assetName = "vRPZ_Ambience",
         category = {"medium", "long"},
+        volume = 0.75,
         loopInterval = 0
     }
 
