ba87eb71ed3202675845e7dcad2e3a2a3d327898
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-05-04 11:14:53 +0300

    Update exports.lua

--- [Library]/assetify_library/files/assets/module/vRPZ_Core/dep/inventory/shared/exports.lua
+++ [Library]/assetify_library/files/assets/module/vRPZ_Core/dep/inventory/shared/exports.lua
@@ -3,32 +3,19 @@
 -----------------
 
 CGame.createExports({
-    {"fetchInventory", "CInventory", "fetch"},
-    {"ensureInventoryItems", "CInventory", "ensureItems"},
-    {"createInventory", "CInventory", "create"},
-    {"deleteInventory", "CInventory", "delete"},
-    {"setInventoryData", "CInventory", "setData"},
-    {"getInventoryData", "CInventory", "getData"},
-    {"addInventoryItem", "CInventory", "addItem"},
-    {"removeInventoryItem", "CInventory", "removeItem"},
-    {"setInventoryItemProperty", "CInventory", "setItemProperty"},
-    {"getInventoryItemProperty", "CInventory", "getItemProperty"},
-    {"setInventoryItemData", "CInventory", "setItemData"},
-    {"getInventoryItemData", "CInventory", "getItemData"}
+    {"fetchInventoryItem", "CInventory", "fetchItem"},
+    {"fetchInventoryItemSlot", "CInventory", "fetchItemSlot"},
+    {"fetchInventoryItemName", "CInventory", "fetchItemName"},
+    {"fetchInventoryItemWeight", "CInventory", "fetchItemWeight"},
+    {"fetchInventoryItemObjectID", "CInventory", "fetchItemObjectID"},
+    {"fetchInventoryWeaponSlot", "CInventory", "fetchWeaponSlot"},
+    {"fetchInventoryWeaponID", "CInventory", "fetchWeaponID"},
+    {"fetchInventoryWeaponAmmo", "CInventory", "fetchWeaponAmmo"},
+    {"fetchInventoryWeaponMag", "CInventory", "fetchWeaponMag"},
+    {"fetchInventorySlot", "CInventory", "fetchSlot"},
+    {"fetchInventoryItemCount", "CInventory", "fetchItemCount"},
+    {"fetchInventoryMaxSlotsMultiplier", "CInventory", "fetchMaxSlotsMultiplier"},
+    {"fetchInventoryParentMaxSlots", "CInventory", "fetchParentMaxSlots"},
+    {"fetchInventoryParentAssignedSlots", "CInventory", "fetchParentAssignedSlots"},
+    {"fetchInventoryParentUsedSlots", "CInventory", "fetchParentUsedSlots"}
 })
-
-function fetchInventoryItem(...) return CInventory.fetchItem(...) end
-function fetchInventoryItemSlot(...) return CInventory.fetchItemSlot(...) end
-function fetchInventoryItemName(...) return CInventory.fetchItemName(...) end
-function fetchInventoryItemWeight(...) return CInventory.fetchItemWeight(...) end
-function fetchInventoryItemObjectID(...) return CInventory.fetchItemObjectID(...) end
-function fetchInventoryWeaponSlot(...) return CInventory.fetchWeaponSlot(...) end
-function fetchInventoryWeaponID(...) return CInventory.fetchWeaponID(...) end
-function fetchInventoryWeaponAmmo(...) return CInventory.fetchWeaponAmmo(...) end
-function fetchInventoryWeaponMag(...) return CInventory.fetchWeaponMag(...) end
-function fetchInventorySlot(...) return CInventory.fetchSlot(...) end
-function fetchInventoryItemCount(...) return CInventory.fetchItemCount(...) end
-function fetchInventoryMaxSlotsMultiplier(...) return CInventory.fetchMaxSlotsMultiplier(...) end
-function fetchInventoryParentMaxSlots(...) return CInventory.fetchParentMaxSlots(...) end
-function fetchInventoryParentAssignedSlots(...) return CInventory.fetchParentAssignedSlots(...) end
-function fetchInventoryParentUsedSlots(...) return CInventory.fetchParentUsedSlots(...) end
