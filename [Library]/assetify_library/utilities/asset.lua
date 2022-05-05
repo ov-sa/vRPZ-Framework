a73aca427ec8030ac49baf556b65324fd45f9a80
Aviril <89867985+Aviril@users.noreply.github.com>
2022-05-05 20:00:55 +0300

    Update asset.lua

--- [Library]/assetify_library/utilities/asset.lua
+++ [Library]/assetify_library/utilities/asset.lua
@@ -49,9 +49,6 @@ local imports = {
 ----------------------
 
 asset = {
-    cache = {
-        modelID = {}
-    },
     references = {
         root = ((downloadSettings.isAccessSafe and "@") or "").."files/assets/",
         manifest = "manifest",
@@ -202,7 +199,6 @@ if localPlayer then
                     modelID = modelID,
                     collisionID = collisionID
                 }
-                asset.cache.modelID[modelID] = {type = assetType, name = assetName}
                 loadState = true
             end
         end
@@ -217,7 +213,6 @@ if localPlayer then
         if not rwCache then return false end
         if self.synced then
             if self.synced.modelID then
-                asset.cache.modelID[(self.synced.modelID)] = nil
                 imports.engineFreeModel(self.synced.modelID)
             end
             if self.synced.collisionID then
