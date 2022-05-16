18a0bf99204b47372f41d4e8fcec4e9eff4116f3
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-05-16 14:47:28 +0300

    Update inventory.lua

--- [Library]/assetify_library/files/assets/module/vRPZ_Config/dep/templates/inventory.lua
+++ [Library]/assetify_library/files/assets/module/vRPZ_Config/dep/templates/inventory.lua
@@ -21,42 +21,82 @@ FRAMEWORK_CONFIGS["Templates"]["Inventory"] = {
     ["Slots"] = {
         ["Primary"] = {
             identifier = "Weapon",
-            slots = {rows = 2, columns = 6}
+            slots = {rows = 2, columns = 6},
+            ["EN"] = "Primary",
+            ["TR"] = "Öncelik",
+            ["RU"] = "Начальный",
+            ["BR"] = "Primária",
+            ["NL"] = "primair"
         },
     
         ["Secondary"] = {
             identifier = "Weapon",
-            slots = {rows = 2, columns = 4}
+            slots = {rows = 2, columns = 4},
+            ["EN"] = "Secondary",
+            ["TR"] = "İkincil",
+            ["RU"] = "Среднее",
+            ["BR"] = "Secundária",
+            ["NL"] = "Ondergeschikt"
         },
     
         ["Helmet"] = {
             identifier = "Helmet",
-            slots = {rows = 2, columns = 2}
+            slots = {rows = 2, columns = 2},
+            ["EN"] = "Helmet",
+            ["TR"] = "Kask",
+            ["RU"] = "Шлем",
+            ["BR"] = "Capacete",
+            ["NL"] = "Helm"
         },
     
         ["Vest"] = {
             identifier = "Vest",
-            slots = {rows = 2, columns = 2}
+            slots = {rows = 2, columns = 2},
+            ["EN"] = "Vest",
+            ["TR"] = "Yelek",
+            ["RU"] = "Жилет",
+            ["BR"] = "Colete",
+            ["NL"] = "Vest"
         },
     
         ["Backpack"] = {
             identifier = "Backpack",
-            slots = {rows = 3, columns = 3}
+            slots = {rows = 3, columns = 3},
+            ["EN"] = "Backpack",
+            ["TR"] = "Sırt çantası",
+            ["RU"] = "Рюкзак",
+            ["BR"] = "Mochila",
+            ["NL"] = "Rugzak"
         },
 
         ["Upper"] = {
             identifier = "Upper",
-            slots = {rows = 2, columns = 2}
+            slots = {rows = 2, columns = 2},
+            ["EN"] = "Male",
+            ["TR"] = "Erkek",
+            ["RU"] = "Мужской",
+            ["BR"] = "Masculino",
+            ["NL"] = "Mannelijk"
         },
 
         ["Lower"] = {
             identifier = "Lower",
-            slots = {rows = 2, columns = 2}
+            slots = {rows = 2, columns = 2},
+            ["EN"] = "Male",
+            ["TR"] = "Erkek",
+            ["RU"] = "Мужской",
+            ["BR"] = "Masculino",
+            ["NL"] = "Mannelijk"
         },
 
         ["Shoes"] = {
             identifier = "Shoes",
-            slots = {rows = 2, columns = 2}
+            slots = {rows = 2, columns = 2},
+            ["EN"] = "Male",
+            ["TR"] = "Erkek",
+            ["RU"] = "Мужской",
+            ["BR"] = "Masculino",
+            ["NL"] = "Mannelijk"
         }
     }
 }
