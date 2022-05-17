7639b5e0da8e0b8253ea9843f2bcd41809aefaaa
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-05-17 12:33:05 +0300

    Update inventory.lua

--- [Handler]/player_handler/handlers/inventory.lua
+++ [Handler]/player_handler/handlers/inventory.lua
@@ -25,18 +25,17 @@ local imports = {
 -------------------------------
 
 imports.addEvent("Player:onAddItem", true)
-imports.addEventHandler("Player:onAddItem", root, function(item, parent, prevSlot, newSlot)
+imports.addEventHandler("Player:onAddItem", root, function(parent, item, prevSlot, newSlot, amount)
     if not CPlayer.isInitialized(source) then return false end
-    prevSlot, newSlot = imports.tonumber(prevSlot), imports.tonumber(newSlot)
+    prevSlot, newSlot, amount = imports.tonumber(prevSlot), imports.tonumber(newSlot), imports.tonumber(amount)
     local characterID = CPlayer.getCharacterID(source)
     local inventoryID = CPlayer.getInventoryID(source)
 
     print("WOOW 1: "..tostring(item).." : "..tostring(prevSlot).." : "..tostring(newSlot))
-    if item and prevSlot and newSlot and CInventory.isSlotAvailableForOrdering(source, item, newSlot) then
-        --TODO: ADD FUNCTION TO GIVE/REVOKE ITEMS ETC
+    if item and prevSlot and newSlot and amount and CInventory.isSlotAvailableForOrdering(source, item, newSlot) then
         print("WOOW 2")
-        local itemAmount = CInventory.fetchItemCount(item)
-        setElementData(localPlayer, "Item:"..item, itemAmounts + 1) --SYNC AMOUNT TOO....
+        addItemCount(source, item, amount)
+        removeItemCount(parent, item, amount)
         --TODO: WHY IS IT CLEARING WHEN VICINITY TO INVENTORY?
         --playerInventorySlots[source].slots[prevSlot] = nil --TODO: ONLY FOR ORDER
         CInventory.CBuffer[inventoryID].slots[newSlot] = {
