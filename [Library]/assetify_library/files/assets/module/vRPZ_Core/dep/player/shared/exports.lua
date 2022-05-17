c87f99de9900f0b905dd56e63efcb615d7cc0f73
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-05-17 11:37:24 +0300

    Добавлена ​​функция для получения идентификатора персонажа

--- [Library]/assetify_library/files/assets/module/vRPZ_Core/dep/player/shared/exports.lua
+++ [Library]/assetify_library/files/assets/module/vRPZ_Core/dep/player/shared/exports.lua
@@ -5,6 +5,7 @@
 CGame.createExports({
     {exportName = "isPlayerInitialized", moduleName = "CPlayer", moduleMethod = "isInitialized"},
     {exportName = "getLoggedPlayers", moduleName = "CPlayer", moduleMethod = "getLogged"},
+    {exportName = "getPlayerCharacterID", moduleName = "CPlayer", moduleMethod = "getCharacterID"},
     {exportName = "getPlayerChannel", moduleName = "CPlayer", moduleMethod = "getChannel"},
     {exportName = "getPlayerParty", moduleName = "CPlayer", moduleMethod = "getParty"}
 })
