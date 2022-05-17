e01a9f72d79fd6493b44e7696b1ad02271334244
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-05-17 12:35:14 +0300

    Update inventory.lua

--- [Handler]/player_handler/handlers/inventory.lua
+++ [Handler]/player_handler/handlers/inventory.lua
@@ -25,24 +25,23 @@ local imports = {
 -------------------------------
 
 imports.addEvent("Player:onAddItem", true)
-imports.addEventHandler("Player:onAddItem", root, function(parent, item, prevSlot, newSlot, amount)
+imports.addEventHandler("Player:onAddItem", root, function(parent, item, prevSlot, newSlot)
     if not CPlayer.isInitialized(source) then return false end
-    prevSlot, newSlot, amount = imports.tonumber(prevSlot), imports.tonumber(newSlot), imports.tonumber(amount)
+    prevSlot, newSlot = imports.tonumber(prevSlot), imports.tonumber(newSlot)
+
     local characterID = CPlayer.getCharacterID(source)
     local inventoryID = CPlayer.getInventoryID(source)
-
     print("WOOW 1: "..tostring(item).." : "..tostring(prevSlot).." : "..tostring(newSlot))
-    if item and prevSlot and newSlot and amount and CInventory.isSlotAvailableForOrdering(source, item, newSlot) then
+    if item and prevSlot and newSlot and CInventory.isSlotAvailableForOrdering(source, item, newSlot) then
         print("WOOW 2")
-        addItemCount(source, item, amount)
-        removeItemCount(parent, item, amount)
+        addItemCount(source, item, 1)
+        removeItemCount(parent, item, 1)
         --TODO: WHY IS IT CLEARING WHEN VICINITY TO INVENTORY?
         --playerInventorySlots[source].slots[prevSlot] = nil --TODO: ONLY FOR ORDER
         CInventory.CBuffer[inventoryID].slots[newSlot] = {
             item = item
         }
     end
-    --TODO: MAKE FUNCTON TO GET PLAYER;'S INVENTORY ID' and CHAR ID
     imports.triggerClientEvent(source, "Client:onSyncInventoryBuffer", source, CInventory.CBuffer[inventoryID])
 end)
 
@@ -53,5 +52,16 @@ end)
 
 imports.addEvent("Player:onOrderItem", true)
 imports.addEventHandler("Player:onOrderItem", root, function(item, prevSlot, newSlot)
+    if not CPlayer.isInitialized(source) then return false end
+    prevSlot, newSlot = imports.tonumber(prevSlot), imports.tonumber(newSlot)
 
+    local characterID = CPlayer.getCharacterID(source)
+    local inventoryID = CPlayer.getInventoryID(source)
+    if item and prevSlot and newSlot and CInventory.isSlotAvailableForOrdering(source, item, newSlot) then
+        CInventory.CBuffer[inventoryID].slots[prevSlot] = nil
+        CInventory.CBuffer[inventoryID].slots[newSlot] = {
+            item = item
+        }
+    end
+    imports.triggerClientEvent(source, "Client:onSyncInventoryBuffer", source, CInventory.CBuffer[inventoryID])
 end)
