0aed51cc7538869e17d9bf7791922fac35eb10e4
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-05-17 12:12:25 +0300

    Update index.lua

--- [Library]/assetify_library/files/assets/module/vRPZ_Core/dep/inventory/server/index.lua
+++ [Library]/assetify_library/files/assets/module/vRPZ_Core/dep/inventory/server/index.lua
@@ -5,6 +5,8 @@
 local imports = {
     pairs = pairs,
     tonumber = tonumber,
+    getElementType = getElementType,
+    math = math,
     string = string,
     assetify = assetify,
     dbify = dbify
@@ -33,13 +35,32 @@ CInventory.getItemProperty = imports.dbify.inventory.item.getProperty
 CInventory.setItemData = imports.dbify.inventory.item.setData
 CInventory.getItemData = imports.dbify.inventory.item.getData
 
+CInventory.fetchParentMaxSlots = function(parent)
+    if imports.getElementType(parent) == "player" then
+        if not CPlayer.isInitialized(parent) then return false end
+        local inventoryID = CPlayer.getInventoryID(parent)
+        return imports.math.max(CInventory.fetchMaxSlotsMultiplier(), (inventoryID and CInventory.CBuffer[inventoryID].maxSlots) or 0)
+    end
+end
+
+CInventory.fetchParentAssignedSlots = function(parent)
+    if imports.getElementType(parent) == "player" then
+        if not CPlayer.isInitialized(parent) then return false end
+        local inventoryID = CPlayer.getInventoryID(parent)
+        return (inventoryID and CInventory.CBuffer[inventoryID].slots) or false
+    end
+end
+
 CInventory.isSlotAvailableForOrdering = function(player, item, slot, isEquipped)
     slot = imports.tonumber(slot)
+    print("test 1")
     if not CPlayer.isInitialized(player) or not item or not slot then return false end
     local itemData = CInventory.fetchItem(item)
     if not itemData then return false end
+    print("test 2")
     local maxSlots, usedSlots = CInventory.fetchParentMaxSlots(player), CInventory.fetchParentUsedSlots(player)
     if not maxSlots or not usedSlots or (slot > maxSlots) or usedSlots[slot] then return false end
+    print("test 3")
     if not isEquipped then
         --TODO: ...
         --local usedSlots = getElementUsedSlots(player)
@@ -47,18 +68,22 @@ CInventory.isSlotAvailableForOrdering = function(player, item, slot, isEquipped)
     end
     local slotRow, slotColumn = CInventory.fetchSlotLocation(slot)
     if (itemData.data.itemWeight.columns - 1) > (FRAMEWORK_CONFIGS["UI"]["Inventory"].inventory.columns - slotColumn) then return false end
+    print("test 4")
     for i = slot, slot + (itemData.data.itemWeight.columns - 1), 1 do
         if (i > maxSlots) or usedSlots[i] then
+            print("test 5")
             return false
         else
             for k = 2, itemData.data.itemWeight.rows, 1 do
                 local v = i + (FRAMEWORK_CONFIGS["UI"]["Inventory"].inventory.columns*(k - 1))
                 if (v > maxSlots) or usedSlots[v] then
+                    print("test 6")
                     return false
                 end
             end
         end
     end
+    print("test 7")
     return true
 end
 
