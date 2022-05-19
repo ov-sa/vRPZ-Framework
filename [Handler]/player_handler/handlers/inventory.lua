f0075968d4abdb950858f8cdd33cab87062baeb7
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-05-19 11:28:30 +0300

    Update inventory.lua

--- [Handler]/player_handler/handlers/inventory.lua
+++ [Handler]/player_handler/handlers/inventory.lua
@@ -1,10 +1,11 @@
 --TODO: REMOVE LATER
 
+local testPed = createPed(0, 0, 0, 0)
+setElementAlpha(testPed, 0)
+setElementData(testPed, "Loot:Type", "something")
+setElementData(testPed, "Loot:Name", "Test Loot")
+
 CGame.execOnModuleLoad(function()
-    local testPed = createPed(0, 0, 0, 0)
-    setElementAlpha(testPed, 0)
-    setElementData(testPed, "Loot:Type", "something")
-    setElementData(testPed, "Loot:Name", "Test Loot")
     for i, j in pairs(CInventory.CItems) do
         CInventory.addItemCount(testPed, i, 1)
     end
