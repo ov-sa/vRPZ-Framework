e776c9901e595523325edd5f3f0b4f8868d4b495
Aviril <89867985+Aviril@users.noreply.github.com>
2022-05-29 23:58:02 +0300

    Update planar.lua

--- [Library]/assetify_library/utilities/lights/planar.lua
+++ [Library]/assetify_library/utilities/lights/planar.lua
@@ -7,14 +7,13 @@
      Desc: Planar Light Utilities ]]--
 ----------------------------------------------------------------
 
---TODO: WIP...
+
 -----------------
 --[[ Imports ]]--
 -----------------
 
 local imports = {
     pairs = pairs,
-    tonumber = tonumber,
     isElement = isElement,
     destroyElement = destroyElement,
     setmetatable = setmetatable,
@@ -37,23 +36,23 @@ local imports = {
 
 light.planar = {
     cache = {
-        validModels = {
-            {index = "planar_1x1", texName = "assetify_light_plane"}
+        validTypes = {
+            {index = "planar_1x1", textureName = "assetify_light_plane"}
         }
     },
     buffer = {}
 }
-for i = 1, #light.planar.validModels, 1 do
-    local j = light.planar.validModels[i]
+for i = 1, #light.planar.validTypes, 1 do
+    local j = light.planar.validTypes[i]
     local modelID = imports.engineRequestModel("object")
     local modelPath = "utilities/rw/"..j.index.."/"
     j.modelID = modelID
     imports.engineImportTXD(imports.engineLoadTXD(modelPath.."dict.rw"), modelID)
     imports.engineReplaceModel(imports.engineLoadDFF(modelPath.."buffer.rw"), modelID, true)
     imports.engineReplaceCOL(imports.engineLoadCOL(modelPath.."collision.rw"), modelID)
-    light.planar.validModels[i] = nil
-    light.planar.validModels[(j.index)] = j
-    light.planar.validModels[(j.index)].index = nil
+    light.planar.validTypes[i] = nil
+    light.planar.validTypes[(j.index)] = j
+    light.planar.validTypes[(j.index)].index = nil
 end
 light.planar.__index = light.planar
 
@@ -78,44 +77,22 @@ if localPlayer then
         return true
     end
 
-    function light.planar:load(element, shaderCategory, shaderName, textureName, shaderTextures, shaderInputs, rwCache, shaderMaps, encryptKey, shaderPriority, shaderDistance)
+    function light.planar:load(lightType, lightPosition, shaderInputs)
         if not self or (self == light.planar) then return false end
-        local isExternalResource = sourceResource and (sourceResource ~= resource)
-        if not shaderCategory or not shaderName or (isExternalResource and light.planar.cache.remoteBlacklist[shaderName]) or (not light.planar.preLoaded[shaderName] and not light.planar.rwCache[shaderName]) or not textureName or not shaderTextures or not shaderInputs or not rwCache or not shaderMaps then return false end
-        element = ((element and imports.isElement(element)) and element) or false
-        shaderPriority = imports.tonumber(shaderPriority) or light.planar.cache.shaderPriority
-        shaderDistance = imports.tonumber(shaderDistance) or light.planar.cache.shaderDistance
-        self.isPreLoaded = (light.planar.preLoaded[shaderName] and true) or false
-        self.cLight = (self.isPreLoaded and light.planar.preLoaded[shaderName])
-        if not self.cLight then
-            self.cLight = imports.dxCreateShader(light.planar.rwCache[shaderName](shaderMaps), shaderPriority, shaderDistance, false, "all")
-            renderer:setServerTick(_, self.cLight, syncer.librarySerial)
-        end
-        light.planar.buffer[(self.cLight)] = true
-        if not self.isPreLoaded then rwCache.light.planar[textureName] = self.cLight end
-        for i, j in imports.pairs(shaderTextures) do
-            if j and imports.isElement(rwCache.texture[j]) then
-                imports.dxSetShaderValue(self.cLight, i, rwCache.texture[j])
-            end
-        end
+        if not lightType or not light.planar.validTypes[lightType] then return false end
+        local cShader = imports.dxCreateShader(light.planar.rwCache["Assetify_LightPlanar"](shaderMaps), shader.cache.shaderPriority, shader.cache.shaderDistance, false, "all")
+        self.cLight = false -- TODO: CREATE DUMMY HERE...
+        --TODO: CREATE DUMMY HERE...
+        shader.buffer.shader[cShader] = "light"
+        self.lightType = lightType
         for i, j in imports.pairs(shaderInputs) do
             imports.dxSetShaderValue(self.cLight, i, j)
         end
         self.shaderData = {
-            element = element,
-            shaderCategory = shaderCategory,
-            shaderName = shaderName,
-            textureName = textureName,
-            shaderTextures = shaderTextures,
-            shaderInputs = shaderInputs,
-            shaderPriority = shaderPriority,
-            shaderDistance = shaderDistance
+            element = cShader,
+            shaderInputs = shaderInputs
         }
-        light.planar.buffer[(self.shaderData.element)] = light.planar.buffer[(self.shaderData.element)] or {}
-        local bufferCache = light.planar.buffer[(self.shaderData.element)]
-        bufferCache[shaderCategory] = bufferCache[shaderCategory] or {}
-        bufferCache[shaderCategory][textureName] = self
-        imports.engineApplyShaderToWorldTexture(self.cLight, textureName, element or nil)
+        imports.engineApplyShaderToWorldTexture(cShader, light.planar.validTypes[lightType].textureName, self.cLight)
         return true
     end
 
@@ -128,6 +105,7 @@ if localPlayer then
                 imports.destroyElement(self.cLight)
             end
             if self.shaderData.element and imports.isElement(self.shaderData.element) then
+                shader.buffer.shader[(self.shaderData.element)] = nil
                 imports.destroyElement(self.shaderData.element)
             end
         end
