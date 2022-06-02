ae510cc9fe38699fee9ee07b9d053ee82ae74843
Aviril <89867985+Aviril@users.noreply.github.com>
2022-06-02 11:42:00 +0300

    Update vehicle.lua

--- [Library]/assetify_library/files/assets/module/DBify_Library/dep/module/vehicle.lua
+++ [Library]/assetify_library/files/assets/module/DBify_Library/dep/module/vehicle.lua
@@ -4,6 +4,7 @@
 
 local imports = {
     type = type,
+    unpack = unpack,
     addEventHandler = addEventHandler,
     dbQuery = dbQuery,
     dbPoll = dbPoll,
@@ -21,23 +22,18 @@ dbify.vehicle = {
         table = "dbify_vehicles",
         keyColumn = "id"
     },
-    async = {
-        fetchAll = 2,
-        create = 1,
-        delete = 2,
-        setData = 3,
-        getData = 3
-    },
 
     fetchAll = function(...)
         if not dbify.mysql.connection.instance then return false end
-        local keyColumns, callback, ... = dbify.parse(2, ...)
-        return dbify.mysql.table.fetchContents(dbify.vehicle.connection.table, keyColumns, callback, ...)
+        local cArgs = {dbify.parseArgs(2, ...)}
+        local keyColumns, callback = dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs)
+        return dbify.mysql.table.fetchContents(dbify.vehicle.connection.table, keyColumns, callback, imports.unpack(cArgs))
     end,
 
     create = function(...)
         if not dbify.mysql.connection.instance then return false end
-        local callback, ... = dbify.parse(1, ...)
+        local cArgs = {dbify.parseArgs(1, ...)}
+        local callback = dbify.fetchArg(_, cArgs)
         if not callback or (imports.type(callback) ~= "function") then return false end
         imports.dbQuery(function(queryHandler, arguments)
             local callbackReference = callback
@@ -46,13 +42,14 @@ dbify.vehicle = {
             if callbackReference and (imports.type(callbackReference) == "function") then
                 callbackReference(result, arguments)
             end
-        end, {{...}}, dbify.mysql.connection.instance, "INSERT INTO `??` (`??`) VALUES(NULL)", dbify.vehicle.connection.table, dbify.vehicle.connection.keyColumn)
+        end, {cArgs}, dbify.mysql.connection.instance, "INSERT INTO `??` (`??`) VALUES(NULL)", dbify.vehicle.connection.table, dbify.vehicle.connection.keyColumn)
         return true
     end,
 
     delete = function(...)
         if not dbify.mysql.connection.instance then return false end
-        local vehicleID, callback, ... = dbify.parse(2, ...)
+        local cArgs = {dbify.parseArgs(2, ...)}
+        local vehicleID, callback = dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs)
         if not vehicleID or (imports.type(vehicleID) ~= "number") then return false end
         return dbify.vehicle.getData(vehicleID, {dbify.vehicle.connection.keyColumn}, function(result, arguments)
             local callbackReference = callback
@@ -66,25 +63,27 @@ dbify.vehicle = {
                     callbackReference(false, arguments)
                 end
             end
-        end, ...)
+        end, imports.unpack(cArgs))
     end,
 
     setData = function(...)
         if not dbify.mysql.connection.instance then return false end
-        local vehicleID, dataColumns, callback, ... = dbify.parse(3, ...)
+        local cArgs = {dbify.parseArgs(3, ...)}
+        local vehicleID, dataColumns, callback = dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs)
         if not vehicleID or (imports.type(vehicleID) ~= "number") or not dataColumns or (imports.type(dataColumns) ~= "table") or (#dataColumns <= 0) then return false end
         return dbify.mysql.data.set(dbify.vehicle.connection.table, dataColumns, {
             {dbify.vehicle.connection.keyColumn, vehicleID}
-        }, callback, ...)
+        }, callback, imports.unpack(cArgs))
     end,
 
     getData = function(...)
         if not dbify.mysql.connection.instance then return false end
-        local vehicleID, dataColumns, callback, ... = dbify.parse(3, ...)
+        local cArgs = {dbify.parseArgs(3, ...)}
+        local vehicleID, dataColumns, callback = dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs), dbify.fetchArg(_, cArgs)
         if not vehicleID or (imports.type(vehicleID) ~= "number") or not dataColumns or (imports.type(dataColumns) ~= "table") or (#dataColumns <= 0) then return false end
         return dbify.mysql.data.get(dbify.vehicle.connection.table, dataColumns, {
             {dbify.vehicle.connection.keyColumn, vehicleID}
-        }, true, callback, ...)
+        }, true, callback, imports.unpack(cArgs))
     end
 }
 
