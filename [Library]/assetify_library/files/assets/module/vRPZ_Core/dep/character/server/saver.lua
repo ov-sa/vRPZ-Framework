c34ada499f3ac79c2b7d5a84a208c8feaad092df
Aviril <89867985+Aviril@users.noreply.github.com>
2022-06-03 00:09:10 +0300

    Update saver.lua

--- [Library]/assetify_library/files/assets/module/vRPZ_Core/dep/character/server/saver.lua
+++ [Library]/assetify_library/files/assets/module/vRPZ_Core/dep/character/server/saver.lua
@@ -15,7 +15,8 @@ local imports = {
     json = json,
     table = table,
     math = math,
-    dbify = dbify
+    dbify = dbify,
+    thread = thread
 }
 
 
@@ -23,89 +24,74 @@ local imports = {
 --[[ Module: Character ]]--
 ---------------------------
 
---TODO: ADD ASYNC SUPPORT FOR ALL THESE...
 CCharacter.resetProgress = function(player, isForceReset, depDatas, saveProgress, loadProgress)
-    if isForceReset then
-        imports.setElementData(player, "Player:Initialized", nil)
-        imports.setElementData(player, "Character:ID", nil)
-    end
-    if CPlayer.CAttachments[player] then
-        for i, j in imports.pairs(CPlayer.CAttachments[player]) do
-            if j and imports.isElement(j) then
-                imports.destroyElement(j)
+    return imports.thread:create(function(self)
+        if isForceReset then
+            imports.setElementData(player, "Player:Initialized", nil)
+            imports.setElementData(player, "Character:ID", nil)
+        end
+        if CPlayer.CAttachments[player] then
+            for i, j in imports.pairs(CPlayer.CAttachments[player]) do
+                if j and imports.isElement(j) then
+                    imports.destroyElement(j)
+                end
             end
         end
-    end
-    CPlayer.CChannel[player] = nil
-    CPlayer.CAttachments[player] = nil
-    local dataBuffer = {
-        player = {},
-        character = {},
-        inventory = {}
-    }
-    if isForceReset then
-        for i = 1, #FRAMEWORK_CONFIGS["Player"]["Datas"], 1 do
-            local j = FRAMEWORK_CONFIGS["Player"]["Datas"][i]
-            dataBuffer.player[i] = {j, imports.getElementData(player, "Player:"..j)}
-            imports.setElementData(player, "Player:Data:"..j, nil)
+        CPlayer.CChannel[player] = nil
+        CPlayer.CAttachments[player] = nil
+        local buffer = {player = {}, character = {}, inventory = {}}
+        if isForceReset then
+            for i = 1, #FRAMEWORK_CONFIGS["Player"]["Datas"], 1 do
+                local j = FRAMEWORK_CONFIGS["Player"]["Datas"][i]
+                buffer.player[i] = {j, imports.getElementData(player, "Player:"..j)}
+                imports.setElementData(player, "Player:Data:"..j, nil)
+            end
+            imports.setElementData(player, "Character:Identity", nil)
         end
-        imports.setElementData(player, "Character:Identity", nil)
-    end
-    for i = 1, #FRAMEWORK_CONFIGS["Character"]["Datas"], 1 do
-        local j = FRAMEWORK_CONFIGS["Character"]["Datas"][i]
-        dataBuffer.character[i] = {j, imports.getElementData(player, "Character:Data:"..j)}
-        imports.setElementData(player, "Character:"..j, nil)
-    end
-    for i, j in imports.pairs(FRAMEWORK_CONFIGS["Templates"]["Inventory"]["Slots"]) do
-        imports.setElementData(player, "Slot:"..i, nil)
-        imports.setElementData(player, "Slot:Object:"..i, nil)
-    end
-    imports.table.insert(dataBuffer.inventory, {"max_slots", CInventory.CBuffer[(depDatas.inventoryID)].maxSlots})
-    imports.table.insert(dataBuffer.inventory, {"slots", imports.json.encode(CInventory.CBuffer[(depDatas.inventoryID)].slots)})
-    for i, j in imports.pairs(CInventory.CItems) do
+        for i = 1, #FRAMEWORK_CONFIGS["Character"]["Datas"], 1 do
+            local j = FRAMEWORK_CONFIGS["Character"]["Datas"][i]
+            buffer.character[i] = {j, imports.getElementData(player, "Character:Data:"..j)}
+            imports.setElementData(player, "Character:"..j, nil)
+        end
+        for i, j in imports.pairs(FRAMEWORK_CONFIGS["Templates"]["Inventory"]["Slots"]) do
+            imports.setElementData(player, "Slot:"..i, nil)
+            imports.setElementData(player, "Slot:Object:"..i, nil)
+        end
+        imports.table.insert(buffer.inventory, {"max_slots", CInventory.CBuffer[(depDatas.inventoryID)].maxSlots})
+        imports.table.insert(buffer.inventory, {"slots", imports.json.encode(CInventory.CBuffer[(depDatas.inventoryID)].slots)})
+        for i, j in imports.pairs(CInventory.CItems) do
+            if saveProgress then
+                self:await(CInventory.setItemProperty(self, depDatas.inventoryID, {j.ref}, {
+                    {imports.dbify.inventory.connection.itemFormat.counter, imports.math.max(0, imports.tonumber(imports.getElementData(player, "Item:"..i)) or 0)}
+                }))
+            end
+            imports.setElementData(player, "Item:"..i, (loadProgress and 0) or nil)
+        end
         if saveProgress then
-            CInventory.setItemProperty(depDatas.inventoryID, {j.ref}, {
-                {imports.dbify.inventory.connection.itemFormat.counter, imports.math.max(0, imports.tonumber(imports.getElementData(player, "Item:"..i)) or 0)}
-            })
+            self:await(CPlayer.setData(self, depDatas.serial, buffer.player))
+            self:await(CCharacter.setData(self, depDatas.characterID, buffer.character))
+            self:await(CInventory.setData(self, depDatas.inventoryID, buffer.inventory))
+            CPlayer.CBuffer[(depDatas.serial)], CCharacter.CBuffer[(depDatas.characterID)], CInventory.CBuffer[(depDatas.inventoryID)] = nil, nil, nil
+            imports.collectgarbage()
         end
-        imports.setElementData(player, "Item:"..i, (loadProgress and 0) or nil)
-    end
-    if saveProgress then
-        CPlayer.setData(depDatas.serial, dataBuffer.player)
-        CCharacter.setData(depDatas.characterID, dataBuffer.character)
-        CInventory.setData(depDatas.inventoryID, dataBuffer.inventory)
-        CPlayer.CBuffer[(depDatas.serial)], CCharacter.CBuffer[(depDatas.characterID)], CInventory.CBuffer[(depDatas.inventoryID)] = nil, nil, nil
-        imports.collectgarbage()
-    end
-    return true
+    end):resume()
 end
 
-CCharacter.loadInventory = function(player, depDatas, callback)
+CCharacter.loadInventory = function(cThread, player, depDatas)
+    if not cThread then return false end
     if not player or not imports.isElement(player) or (imports.getElementType(player) ~= "player") then return false end
-    CInventory.getItemProperty(depDatas.inventoryID, CInventory.CRefs.index, {imports.dbify.inventory.connection.itemFormat.counter}, function(result, args)
-        local cbRef = callback
-        if not result and (CInventory.CRefs.index > 0) then
-            if callback and (imports.type(callback) == "function") then
-                cbRef(false, args[1], args[2])
-            end
-            return false
-        end
-        CInventory.getData(args[2].inventoryID, {"max_slots", "slots"}, function(result, args)
-            local cbRef = callback
-            result = result or {}
-            result.max_slots, result.slots = imports.math.max(CInventory.fetchMaxSlotsMultiplier(), imports.tonumber(result.max_slots) or 0), (result.slots and imports.json.decode(result.slots)) or {}
-            CInventory.CBuffer[(args[2].characterID)] = {
-                maxSlots = result.max_slots,
-                slots = result.slots
-            }
-            for i, j in imports.pairs(args[3]) do
-                imports.setElementData(args[1], "Item:"..(CInventory.CRefs.ref[i]), imports.tonumber(j[(imports.dbify.inventory.connection.itemFormat.counter)]) or 0)
-            end
-            if callback and (imports.type(callback) == "function") then
-                cbRef(true)
-            end
-        end, args[1], args[2], result)
-    end, true, player, depDatas)
+    local DItemProperty. = cThread:await(CInventory.getItemProperty(cThread, player, depDatas.inventoryID, CInventory.CRefs.index, {imports.dbify.inventory.connection.itemFormat.counter}, true))
+    if not DItemProperty. and (#CInventory.CRefs.index > 0) then return false end
+    local DInventoryProperty = cThread:await(CInventory.getData(cThread, depDatas.inventoryID, {"max_slots", "slots"}))
+    DInventoryProperty = DInventoryProperty or {}
+    DInventoryProperty.max_slots, DInventoryProperty.slots = imports.math.max(CInventory.fetchMaxSlotsMultiplier(), imports.tonumber(DInventoryProperty.max_slots) or 0), (DInventoryProperty.slots and imports.json.decode(DInventoryProperty.slots)) or {}
+    CInventory.CBuffer[depDatas.characterID)] = {
+        maxSlots = DInventoryProperty.max_slots,
+        slots = DInventoryProperty.slots
+    }
+    for i, j in imports.pairs(DItemProperty) do
+        imports.setElementData(player, "Item:"..(CInventory.CRefs.ref[i]), imports.tonumber(j[(imports.dbify.inventory.connection.itemFormat.counter)]) or 0)
+    end
     return true
 end
 
