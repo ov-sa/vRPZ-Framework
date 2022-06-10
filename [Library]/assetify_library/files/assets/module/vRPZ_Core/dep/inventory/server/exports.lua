e1a2ce57bc2b35ed17933af35a04a719902b24f9
Aviril <89867985+Aviril@users.noreply.github.com>
2022-06-10 23:27:02 +0300

    Update exports.lua

--- [Library]/assetify_library/files/assets/module/vRPZ_Core/dep/inventory/server/exports.lua
+++ [Library]/assetify_library/files/assets/module/vRPZ_Core/dep/inventory/server/exports.lua
@@ -32,7 +32,7 @@ CGame.exportModule("CInventory", {
 --[[ Events ]]--
 ----------------
 
-network:create("Player:onAddItem"):on(function(source, vicinity, item, slot)
+imports.network:create("Player:onAddItem"):on(function(source, vicinity, item, slot)
     if not CPlayer.isInitialized(source) then return false end
     local inventoryID = CPlayer.getInventoryID(source)
     if CInventory.isSlotAvailableForOrdering(source, item, _, slot) then
@@ -44,7 +44,7 @@ network:create("Player:onAddItem"):on(function(source, vicinity, item, slot)
     imports.network:emit("Client:onSyncInventoryBuffer", true, false, source, CInventory.CBuffer[inventoryID])
 end)
 
-network:create("Player:onOrderItem"):on(function(source, item, prevSlot, slot)
+imports.network:create("Player:onOrderItem"):on(function(source, item, prevSlot, slot)
     if not CPlayer.isInitialized(source) then return false end
     if FRAMEWORK_CONFIGS["Templates"]["Inventory"]["Slots"][slot] then return CInventory.equipItem(source, item, prevSlot, slot, true) end
     if FRAMEWORK_CONFIGS["Templates"]["Inventory"]["Slots"][prevSlot] then return CInventory.dequipItem(source, item, prevSlot, slot, true) end
@@ -56,7 +56,7 @@ network:create("Player:onOrderItem"):on(function(source, item, prevSlot, slot)
     imports.network:emit("Client:onSyncInventoryBuffer", true, false, source, CInventory.CBuffer[inventoryID])
 end)
 
-network:create("Player:onDropItem"):on(function(source, vicinity, item, amount, prevSlot)
+imports.network:create("Player:onDropItem"):on(function(source, vicinity, item, amount, prevSlot)
     if not CPlayer.isInitialized(source) then return false end
     local inventoryID = CPlayer.getInventoryID(source)
     if prevSlot and CInventory.isVicinityAvailableForDropping(vicinity, item, amount) then
