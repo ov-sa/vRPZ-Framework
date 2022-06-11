b7b22195f89e6c816c3ad69bdef1e7d496bb5828
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-06-11 12:29:55 +0300

    Update exports.lua

--- [Library]/assetify_library/files/assets/module/vRPZ_Core/dep/player/client/exports.lua
+++ [Library]/assetify_library/files/assets/module/vRPZ_Core/dep/player/client/exports.lua
@@ -24,5 +24,5 @@ CGame.exportModule("CPlayer", {
 
 imports.network:create("Player:onLogin"):on(function(source) CPlayer.CLogged[source] = true end)
 imports.network:create("Player:onLogout"):on(function(source) CPlayer.CLogged[source] = nil end)
-imports.network:create("Player:onUpdateChannel"):on(function(...) CPlayer.setChannel(...) end)
-imports.network:create("Player:onUpdateParty"):on(function(...) CPlayer.setParty(...) end)
+imports.network:create("Player:onUpdateChannel"):on(CPlayer.setChannel)
+imports.network:create("Player:onUpdateParty"):on(CPlayer.setParty)
