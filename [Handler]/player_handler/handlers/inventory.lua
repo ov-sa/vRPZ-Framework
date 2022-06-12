c536aa77e9095e98d600be4d3312e6965f9d5a2d
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-06-12 22:58:30 +0300

    Update inventory.lua

--- [Handler]/player_handler/handlers/inventory.lua
+++ [Handler]/player_handler/handlers/inventory.lua
@@ -2,11 +2,11 @@
 
 local testPed = createPed(0, 0, 0, 0)
 setElementAlpha(testPed, 0)
-assetify.syncer.setEntityData(testPed, "Loot:Type", "something")
-assetify.syncer.setEntityData(testPed, "Loot:Name", "Test Loot")
-assetify.syncer.setEntityData(testPed, "Inventory:MaxSlots", 10000)
 
 CGame.execOnModuleLoad(function()
+    CGame.setEntityData(testPed, "Loot:Type", "something")
+    CGame.setEntityData(testPed, "Loot:Name", "Test Loot")
+    CGame.setEntityData(testPed, "Inventory:MaxSlots", 10000)
     for i, j in pairs(CInventory.CItems) do
         CInventory.addItemCount(testPed, i, 1)
     end
