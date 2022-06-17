c82a99c51e0b03034532c51f8632b20ac1f58e0f
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-06-17 12:28:36 +0300

    Update developer list

--- [Library]/assetify_library/utilities/bone.lua
+++ [Library]/assetify_library/utilities/bone.lua
@@ -2,7 +2,7 @@
 --[[ Resource: Assetify Library
      Script: utilities: bone.lua
      Author: vStudio
-     Developer(s): Aviril, Tron
+     Developer(s): Aviril, Tron, Mario, Аниса
      DOC: 19/10/2021
      Desc: Bone Utilities ]]--
 ----------------------------------------------------------------
