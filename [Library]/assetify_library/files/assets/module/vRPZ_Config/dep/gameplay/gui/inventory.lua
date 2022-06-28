622a520c7cd4826fc6090d534342ccf72045088b
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-06-29 02:00:20 +0300

    Improved config

--- [Library]/assetify_library/files/assets/module/vRPZ_Config/dep/gameplay/gui/inventory.lua
+++ [Library]/assetify_library/files/assets/module/vRPZ_Config/dep/gameplay/gui/inventory.lua
@@ -6,19 +6,15 @@ FRAMEWORK_CONFIGS["UI"]["Inventory"] = {
     toggleKey = "j",
     titlebar = {
         height = 35,
-        fontColor = {170, 35, 35, 255},
-        bgColor = {0, 0, 0, 255},
+        fontColor = {170, 35, 35, 255}, bgColor = {0, 0, 0, 255},
         slot = {
             height = 20,
-            fontColor = {0, 0, 0, 255},
-            bgColor = {100, 100, 100, 255}
+            fontColor = {0, 0, 0, 255}, bgColor = {100, 100, 100, 255}
         }
     },
     scroller = {
-        width = 5,
-        thumbHeight = 100,
-        bgColor = {0, 0, 0, 255},
-        thumbColor = {175, 175, 175, 255}
+        width = 5, thumbHeight = 100,
+        thumbColor = {175, 175, 175, 255}, bgColor = {0, 0, 0, 255}
     },
 
     inventory = {
@@ -31,7 +27,7 @@ FRAMEWORK_CONFIGS["UI"]["Inventory"] = {
         },
         rows = 12, columns = 10,
         slotSize = 45, dividerSize = 1,
-        bgColor = {0, 0, 0, 245}, slotColor = {255, 255, 255}, dividerColor = {35, 35, 35},
+        slotColor = {255, 255, 255}, dividerColor = {35, 35, 35}, bgColor = {0, 0, 0, 245},
         slotNameFontColor = {175, 175, 175, 255}, slotCounterFontColor = {100, 100, 100, 255},
         slotAvailableColor = {0, 255, 0}, slotUnavailableColor = {255, 0, 0},
         animDuration = 150
---
+++ [Library]/assetify_library/files/assets/module/vRPZ_Config/dep/gameplay/gui/taskboard.lua
@@ -0,0 +1,21 @@
+------------------
+--[[ Configns ]]--
+------------------
+
+FRAMEWORK_CONFIGS["UI"]["Taskboard"] = {
+    bgColor = {0, 0, 0, 250},
+    titlebar = {
+        height = 27, dividerSize  = 1,
+        fontColor = {175, 175, 175, 255}, dividerColor = {0, 0, 0, 200}, bgColor = {0, 0, 0, 255}
+    },
+    scroller = {
+        width = 2, thumbHeight = 100,
+        thumbColor = {175, 175, 175, 255}, bgColor = {0, 0, 0, 255}
+    },
+
+    ["Contents"] = {
+        height = 27, dividerSize = 2,
+        fontColor = {150, 150, 150, 255}, dividerColor = {15, 15, 15, 200},
+        hoverDuration = 2500
+    }
+}
