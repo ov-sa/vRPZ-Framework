3cb876416da5477cf9b4251a9889ef4b0608bc0f
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-07-05 20:19:36 +0300

    Update shared.lua

--- [Library]/assetify_library/exports/shared.lua
+++ [Library]/assetify_library/exports/shared.lua
@@ -63,7 +63,7 @@ function setGlobalData(...) return syncer:syncGlobalData(...) end
 function getGlobalData(data) if not data or (imports.type(data) ~= "string") then return false end; return syncer.syncedGlobalDatas[data] end
 function setEntityData(...) return syncer:syncEntityData(table:unpack(table:pack(...), 3)) end
 function getEntityData(element, data) if not element or not data or (imports.type(data) ~= "string") then return false end; return syncer.syncedEntityDatas[element] and syncer.syncedEntityDatas[element][data] end
-function createAssetDummy(...) local cDummy = syncer:syncAssetDummy(_, ...); return (cDummy and cDummy.cDummy) or false end
+function createAssetDummy(...) local cDummy = syncer:syncDummySpawn(_, ...); return (cDummy and cDummy.cDummy) or false end
 function setBoneAttachment(...) return syncer:syncBoneAttachment(_, ...) end
 function setBoneDetachment(...) return syncer:syncBoneDetachment(_, ...) end
 function setBoneRefreshment(...) return syncer:syncBoneRefreshment(_, ...) end
