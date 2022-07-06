d1053bd2f2dbb013363668ddbc5f299509d9611c
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-07-06 12:42:00 +0300

    Update shared.lua

--- [Library]/assetify_library/handlers/api/library/shared.lua
+++ [Library]/assetify_library/handlers/api/library/shared.lua
@@ -8,6 +8,15 @@
 ----------------------------------------------------------------
 
 
+-----------------
+--[[ Imports ]]--
+-----------------
+
+local imports = {
+    isElement = isElement
+}
+
+
 -----------------------
 --[[ APIs: Library ]]--
 -----------------------
@@ -21,3 +30,37 @@ end
 function manager.API.Library:isModuleLoaded()
     return syncer.isModuleLoaded
 end
+
+function manager.API.Library:fetchAssets(...)
+    return manager:fetchAssets(...)
+end
+
+function manager.API.Library:getAssetData(...)
+    return manager:getAssetData(...)
+end
+
+function manager.API.Library:getAssetDep(...)
+    return manager:getAssetDep(...)
+end
+
+function manager.API.Library:setElementAsset(...)
+    return syncer:syncElementModel(_, ...)
+end
+
+function manager.API.Library:getElementAssetInfo(element)
+    return syncer:syncElementModel(_, ...)
+    if not element or not imports.isElement(element) or not syncer.syncedElements[element] then return false end
+    return syncer.syncedElements[element].type, syncer.syncedElements[element].name, syncer.syncedElements[element].clump, syncer.syncedElements[element].clumpMaps
+end
+
+
+--TODO: WIP..
+function setGlobalData(...) return syncer:syncGlobalData(...) end
+function getGlobalData(data) return syncer.syncedGlobalDatas[data] end
+function setEntityData(...) return syncer:syncEntityData(table:unpack(table:pack(...), 3)) end
+function getEntityData(element, data) return syncer.syncedEntityDatas[element] and syncer.syncedEntityDatas[element][data] end
+function createAssetDummy(...) local cDummy = syncer:syncDummySpawn(_, ...); return (cDummy and cDummy.cDummy) or false end
+function setBoneAttachment(...) return syncer:syncBoneAttachment(_, ...) end
+function setBoneDetachment(...) return syncer:syncBoneDetachment(_, ...) end
+function setBoneRefreshment(...) return syncer:syncBoneRefreshment(_, ...) end
+function clearBoneAttachment(element) return syncer:syncClearBoneAttachment(_, element) end
