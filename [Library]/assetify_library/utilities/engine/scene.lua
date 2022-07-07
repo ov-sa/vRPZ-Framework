75570934bff2533b68de4d278eb11c215ca110d1
Aviril <89867985+Aviril@users.noreply.github.com>
2022-07-07 11:42:46 +0300

    Update scene.lua

--- [Library]/assetify_library/utilities/engine/scene.lua
+++ [Library]/assetify_library/utilities/engine/scene.lua
@@ -80,15 +80,15 @@ if localPlayer then
         if not scene.public:isInstance(self) then return false end
         if not cAsset or not sceneManifest or not sceneData or not cAsset.synced then return false end
         local posX, posY, posZ, rotX, rotY, rotZ = sceneData.position.x + ((sceneManifest.sceneOffset and sceneManifest.sceneOffset.x) or 0), sceneData.position.y + ((sceneManifest.sceneOffset and sceneManifest.sceneOffset.y) or 0), sceneData.position.z + ((sceneManifest.sceneOffset and sceneManifest.sceneOffset.z) or 0), sceneData.rotation.x, sceneData.rotation.y, sceneData.rotation.z
-        self.cStreamerInstance = imports.createObject(cAsset.synced.modelID, posX, posY, posZ, rotX, rotY, rotZ, (sceneManifest.enableLODs and not cAsset.synced.lodID and cAsset.synced.collisionID and true) or false)
+        self.cStreamerInstance = imports.createObject(cAsset.synced.modelID, posX, posY, posZ, rotX, rotY, rotZ, (sceneManifest.enableLODs and not cAsset.synced.lodID and cAsset.synced.collisionID and true) or false) or false
         imports.setElementDoubleSided(self.cStreamerInstance, true)
         if cAsset.synced.collisionID then
-            self.cCollisionInstance = imports.createObject(cAsset.synced.collisionID, posX, posY, posZ, rotX, rotY, rotZ)
+            self.cCollisionInstance = imports.createObject(cAsset.synced.collisionID, posX, posY, posZ, rotX, rotY, rotZ) or false
             imports.setElementAlpha(self.cCollisionInstance, 0)
             imports.setElementDimension(self.cCollisionInstance, sceneManifest.sceneDimension)
             imports.setElementInterior(self.cCollisionInstance, sceneManifest.sceneInterior)
             if sceneManifest.enableLODs then
-                self.cModelInstance = imports.createObject(cAsset.synced.collisionID, posX, posY, posZ, rotX, rotY, rotZ, true)
+                self.cModelInstance = imports.createObject(cAsset.synced.collisionID, posX, posY, posZ, rotX, rotY, rotZ, true) or false
                 self.cLODInstance = (cAsset.synced.lodID and imports.createObject(cAsset.synced.lodID, posX, posY, posZ, rotX, rotY, rotZ, true)) or false
                 imports.attachElements(self.cModelInstance, self.cCollisionInstance)
                 imports.setElementAlpha(self.cModelInstance, 0)
