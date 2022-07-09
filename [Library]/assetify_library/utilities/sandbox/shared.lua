e1c354e00957e3811dd0b8d7ea70f715824ff831
Aviril <89867985+Aviril@users.noreply.github.com>
2022-07-09 16:17:16 +0300

    Update shared.lua

--- [Library]/assetify_library/utilities/sandbox/shared.lua
+++ [Library]/assetify_library/utilities/sandbox/shared.lua
@@ -34,10 +34,10 @@ destroyElement = function(element) return (isElement(element) and imports.destro
 execFunction = function(exec, ...) if not exec or (imports.type(exec) ~= "function") then return false end return exec(...) end
 
 getElementPosition = function(element, offX, offY, offZ)
+    if not element or not imports.isElement(element) then return false end
     if not offX or not offY or not offZ then
         return imports.getElementPosition(element)
     else
-        if not element or not imports.isElement(element) then return false end
         offX, offY, offZ = imports.tonumber(offX) or 0, imports.tonumber(offY) or 0, imports.tonumber(offZ) or 0
         local cMatrix = imports.getElementMatrix(element)
         return (offX*cMatrix[1][1]) + (offY*cMatrix[2][1]) + (offZ*cMatrix[3][1]) + cMatrix[4][1], (offX*cMatrix[1][2]) + (offY*cMatrix[2][2]) + (offZ*cMatrix[3][2]) + cMatrix[4][2], (offX*cMatrix[1][3]) + (offY*cMatrix[2][3]) + (offZ*cMatrix[3][3]) + cMatrix[4][3]
