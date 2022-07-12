468d46d1b62e7188f53888296ca93ba0029b0500
Aviril <89867985+Aviril@users.noreply.github.com>
2022-07-12 13:25:32 +0300

    Update streamer.lua

--- [Library]/assetify_library/utilities/engine/streamer.lua
+++ [Library]/assetify_library/utilities/engine/streamer.lua
@@ -92,7 +92,6 @@ function streamer.public:resume()
         imports.setElementInterior(self.streamer, self.interior)
     end
     self.isPaused = false
-    --TODO: THIS SHOULD PROBABLY BE DELETED FROM BUFFER
     streamer.private.buffer[(self.dimension)] = streamer.private.buffer[(self.dimension)] or {}
     streamer.private.buffer[(self.dimension)][(self.interior)] = streamer.private.buffer[(self.dimension)][(self.interior)] or {}
     streamer.private.buffer[(self.dimension)][(self.interior)][(self.streamType)] = streamer.private.buffer[(self.dimension)][(self.interior)][(self.streamType)] or {}
