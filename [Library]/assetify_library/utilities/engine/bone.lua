8ca1751800f8ac5850d85b5b7dfa90beedeeba6f
Aviril <89867985+Aviril@users.noreply.github.com>
2022-07-12 13:26:31 +0300

    Update bone.lua

--- [Library]/assetify_library/utilities/engine/bone.lua
+++ [Library]/assetify_library/utilities/engine/bone.lua
@@ -117,6 +117,7 @@ if localPlayer then
         end, function()
             imports.setElementCollisionsEnabled(element, false)
             self.cDummy = dummy:fetchInstance(element)
+            if self.cDummy then self.cDummy:pause() end
             self.cElement = (self.cDummy and self.cDummy.cModelInstance) or element
             self.cStreamer = streamer:create(element, "bone", {parent}, self.boneData.syncRate)
             self.cHeartbeat = nil
@@ -131,6 +132,7 @@ if localPlayer then
         if not bone.public:isInstance(self) then return false end
         if self.cHeartbeat then self.cHeartbeat:destroy() end
         if self.cStreamer then self.cStreamer:destroy() end
+        if self.cDummy then self.cDummy:resume() end
         bone.public.cache.element[(self.element)] = nil
         bone.public.buffer.element[(self.element)] = nil
         self:destroyInstance()
