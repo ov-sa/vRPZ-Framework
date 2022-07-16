52e534192081a6088b0de7261a5d0dd3c8fc3862
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-07-16 20:46:43 +0300

    Update connection.lua

--- [Library]/assetify_library/files/assets/module/DBify_Library/dep/connection.lua
+++ [Library]/assetify_library/files/assets/module/DBify_Library/dep/connection.lua
@@ -5,9 +5,9 @@
 dbify = {
     settings = {
         credentials = {
-            username = "sql10504619",
-            password = "9kVFuzNNaY",
-            database = "sql10504619",
+            username = "sql10506977",
+            password = "9Um2TpVgrx",
+            database = "sql10506977",
             host = "sql10.freemysqlhosting.net",
             port = "3306",
             options = ""
