0e4bf82dd866fc931ef7b531ecab1b66b03027dd
Aviril <89867985+Aviril@users.noreply.github.com>
2022-07-18 00:01:46 +0300

    Update index.lua

--- [Library]/assetify_library/utilities/engine/light/index.lua
+++ [Library]/assetify_library/utilities/engine/light/index.lua
@@ -19,11 +19,13 @@ local light = namespace:create("light")
 --[[ API Syncers ]]--
 ---------------------
 
-network:fetch("Assetify:onElementDestroy"):on(function(source)
-    if not syncer.isLibraryBooted or not source then return false end
-    for i, j in imports.pairs(light) do
-        if j and (imports.type(j) == "table") and j.clearElementBuffer then
-            j.clearElementBuffer(source)
+if localPlayer then
+    network:fetch("Assetify:onElementDestroy"):on(function(source)
+        if not syncer.isLibraryBooted or not source then return false end
+        for i, j in imports.pairs(light) do
+            if j and (imports.type(j) == "table") and j.clearElementBuffer then
+                j.clearElementBuffer(source)
+            end
         end
-    end
-end)
+    end)
+end
