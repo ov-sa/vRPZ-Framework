835182aba21a3fd88fe503e13a54afeebcfb8fb0
Aviril <89867985+Aviril@users.noreply.github.com>
2022-07-20 19:22:37 +0300

    Adding cli cmd to update assetify (WIP)

--- [Library]/assetify_library/handlers/cli.lua
+++ [Library]/assetify_library/handlers/cli.lua
@@ -12,8 +12,11 @@
 --[[ Imports ]]--
 -----------------
 
+local syncer = syncer:import()
 local imports = {
+    fetchRemote = fetchRemote,
     getElementType = getElementType,
+    outputDebugString = outputDebugString,
     addCommandHandler = addCommandHandler
 }
 
@@ -27,8 +30,25 @@ cli.private.validActions = {
     ["update"] = true
 }
 
-function cli.public:update(...)
-    print("EXECUTED UPDATA ACTION")
+function cli.public:update(isBoot, isAutoUpdate, isBackwardsCompatible)
+    if isBoot then
+        imports.fetchRemote(syncer.public.librarySource, function(response, status)
+            if not response or not status or (status ~= 0) then return false end
+            response = table.decode(response)
+            if response and response.tag_name and (syncer.private.libraryVersion ~= response.tag_name) then
+                syncer.private.libraryVersionSource = string.gsub(syncer.private.libraryVersionSource, syncer.private.libraryVersion, response.tag_name, 1)
+                imports.outputDebugString("[Assetify] | "..((settings.library.autoUpdate and "Auto-updating to latest version") or "Latest version available").." - "..response.tag_name, 3)
+                if settings.library.autoUpdate then  cli.public:update(_, true, string.match(syncer.private.libraryVersion, "(%d+)%.") == string.match(response.tag_name, "(%d+)%.")) end
+            end
+        end)
+    else
+        imports.outputDebugString("[Assetify] | "..((isAutoUpdate and "Auto-updating to latest version") or "Latest version available").." - "..response.tag_name, 3)
+        for i = 1, #syncer.private.libraryResources, 1 do
+            local j = syncer.private.libraryResources[i]
+            syncer.private:updateLibrary(j, isBackwardsCompatible)
+        end
+    end
+    return true
 end
 
 
