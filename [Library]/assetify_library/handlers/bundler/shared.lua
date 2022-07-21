956e695becff2e7218667e061a7469512859057f
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-07-21 22:59:33 +0300

    Finalized scheduler

--- [Library]/assetify_library/handlers/bundler/shared.lua
+++ [Library]/assetify_library/handlers/bundler/shared.lua
@@ -99,70 +99,10 @@ bundler.private:createBuffer("core", "__core", [[
     end
 ]])
 
-local validSchedulerTypes = {
-    ["execOnBoot"] = {exec = "assetify.isBooted", network = "Assetify:onBoot"},
-    ["execOnLoad"] = {exec = "assetify.isLoaded", network = "Assetify:onLoad"},
-    ["execOnModuleLoad"] = {exec = "assetify.isModuleLoaded", network = "Assetify:onModuleLoad"}
-}
-function createScheduler()
-    local header, body = [[assetify.scheduler.buffer = {pending = {]], ""
-    local footer = [[
-    local bootExec = function(type)
-        if not assetify.scheduler.buffer.pending[type] then return false end
-        if #assetify.scheduler.buffer.pending[type] > 0 then
-            for i = 1, #assetify.scheduler.buffer.pending[type], 1 do
-                assetify.scheduler.buffer.pending[type][i]()
-            end
-            assetify.scheduler.buffer.pending[type] = {}
-        end
-        return true
-    end
-    local scheduleExec = function(type, exec)
-        if not assetify.scheduler.buffer.schedule[type] then return false end
-        if not exec or (assetify.imports.type(exec) ~= "function") then return false end
-        assetify.imports.table.insert(assetify.scheduler.buffer.schedule[type], exec)
-        return true
-    end  
-    for i, j in assetify.imports.pairs(assetify.scheduler.buffer.schedule) do
-        assetify.scheduler[(assetify.imports.string.gsub(i, "exec", "execSchedule", 1))] = function(...) return scheduleExec(i, ...) end
-    end
-    ]]
-    for i, j in pairs(validSchedulerTypes) do
-        header = header..i..[[ = {}, ]]
-        body = body..[[
-        assetify.scheduler.]]..i..[[ = function(exec)
-            if not exec or (assetify.imports.type(exec) ~= "function") then return false end
-            if ]]..j.exec..[[() then exec()
-            else assetify.imports.table.insert(assetify.scheduler.buffer.pending.]]..i..[[, exec) end
-            return true
-        end
-        ]]
-        footer = footer..[[
-        assetify.network:fetch("]]..j.network..[[", true):on(function() bootExec("]]..i..[[") end, {subscriptionLimit = 1})
-        ]]
-    end
-    header = header..[[}}
-    assetify.scheduler.buffer.schedule = assetify.imports.table.clone(assetify.scheduler.buffer.pending, true)
-    assetify.scheduler.boot = function()
-        for i, j in assetify.imports.pairs(assetify.scheduler.buffer.schedule) do
-            if #j > 0 then
-                for k = 1, #j, 1 do
-                    assetify.scheduler[i](j[k])
-                end
-                assetify.scheduler.buffer.schedule[i] = {}
-            end
-        end
-        return true
-    end
-    ]]
-    return header..body..footer
-end
-createScheduler()
-
 bundler.private:createBuffer("scheduler", _, [[
     ]]..bundler.public:createModule("network")..[[
     assetify.scheduler = {}
-    ]]..createScheduler()..[[
+    ]]..bundler.private:createScheduler()..[[
 ]])
 
 bundler.private:createBuffer("renderer", _, [[
