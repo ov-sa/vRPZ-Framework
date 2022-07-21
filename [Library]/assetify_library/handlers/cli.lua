a1bfa31b5d9a166fff19c09e36ab5fb70713c964
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-07-21 09:18:20 +0300

    Improving cli update cmd

--- [Library]/assetify_library/handlers/cli.lua
+++ [Library]/assetify_library/handlers/cli.lua
@@ -30,32 +30,26 @@ cli.private.validActions = {
     ["update"] = true
 }
 
-function cli.public:update(isAction, isBooted, isBackwardsCompatible)
-    if isBooted then
-        if syncer.private.isLibraryBeingUpdated then return imports.outputDebugString("[Assetify] | An update is already in progress; Kindly have patience...", 3) end
-        syncer.private.isLibraryBeingUpdated = true
-        if isAction then imports.outputDebugString("[Assetify] | Fetching latest version; Hold up...", 3) end
-        imports.fetchRemote(syncer.public.librarySource, function(response, status)
-            if not response or not status or (status ~= 0) then return false end
-            response = table.decode(response)
-            if not response or not response.tag_name then return false end
-            if syncer.private.libraryVersion == response.tag_name then
-                if isAction then imports.outputDebugString("[Assetify] | Already upto date - "..response.tag_name, 3) end
-                return false end
-            syncer.private.libraryVersionSource = string.gsub(syncer.private.libraryVersionSource, syncer.private.libraryVersion, response.tag_name, 1)
-            local isToBeUpdated, isAutoUpdate = (isAction and true) or settings.library.autoUpdate, (not isAction and settings.library.autoUpdate) or false
-            imports.outputDebugString("[Assetify] | "..((isToBeUpdated and not isAutoUpdate and "Updating to latest version") or (isToBeUpdated and isAutoUpdate and "Auto-updating to latest version") or "Latest version available").." - "..response.tag_name, 3)
-            if isToBeUpdated then cli.public:update(isAction, _, string.match(syncer.private.libraryVersion, "(%d+)%.") ~= string.match(response.tag_name, "(%d+)%.")) end
-        end)
-    else
-        for i = 1, #syncer.private.libraryResources, 1 do
-            local j = syncer.private.libraryResources[i]
-            syncer.private:updateLibrary(j, isBackwardsCompatible)
+function cli.public:update(isAction)
+    if syncer.private.isLibraryBeingUpdated then return imports.outputDebugString("[Assetify] | An update is already in progress; Kindly have patience...", 3) end
+    syncer.private.isLibraryBeingUpdated = true
+    if isAction then imports.outputDebugString("[Assetify] | Fetching latest version; Hold up...", 3) end
+    imports.fetchRemote(syncer.public.librarySource, function(response, status)
+        if not response or not status or (status ~= 0) then return false end
+        response = table.decode(response)
+        if not response or not response.tag_name then return false end
+        if syncer.private.libraryVersion == response.tag_name then
+            if isAction then imports.outputDebugString("[Assetify] | Already upto date - "..response.tag_name, 3) end
+            syncer.private.isLibraryBeingUpdated = false
+            return false
         end
-    end
+        syncer.private.libraryVersionSource = string.gsub(syncer.private.libraryVersionSource, syncer.private.libraryVersion, response.tag_name, 1)
+        local isToBeUpdated, isAutoUpdate = (isAction and true) or settings.library.autoUpdate, (not isAction and settings.library.autoUpdate) or false
+        imports.outputDebugString("[Assetify] | "..((isToBeUpdated and not isAutoUpdate and "Updating to latest version") or (isToBeUpdated and isAutoUpdate and "Auto-updating to latest version") or "Latest version available").." - "..response.tag_name, 3)
+        if isToBeUpdated then syncer.private:updateLibrary(string.match(syncer.private.libraryVersion, "(%d+)%.") ~= string.match(response.tag_name, "(%d+)%.")) end
+    end)
     return true
 end
-function cli.private:update() return cli.public:update(true, true) end
 
 
 ---------------------
@@ -65,5 +59,5 @@ function cli.private:update() return cli.public:update(true, true) end
 imports.addCommandHandler("assetify", function(isConsole, _, isAction, ...)
     if not isConsole or (imports.getElementType(isConsole) ~= "console") then return false end
     if not isAction or not cli.private.validActions[isAction] then return false end
-    cli.private[isAction](_, ...)
+    cli.public[isAction](_, true, ...)
 end)
