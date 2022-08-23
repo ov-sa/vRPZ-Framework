1fc6b68d61fb0692619dd2f262f5281d9ea973c9
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-08-23 12:52:32 +0300

    Update vcl.lua

--- [Library]/assetify_library/utilities/sandbox/vcl.lua
+++ [Library]/assetify_library/utilities/sandbox/vcl.lua
@@ -39,82 +39,82 @@ end
 
 function vcl.private.parse(buffer, index, isChild)
     index = index or 1
-    local __P = {
+    local __p = {
         isType = (not isChild and "object") or false,
         isParsed = (not isChild and true) or false, isErrored = "Failed to parse vcl. [Line: %s] [Reason: %s]",
         ref = (isChild and index) or false, index = "", pointer = {}, value = ""
     }
     while(index <= #buffer) do
         local char = vcl.private.fetch(buffer, index)
-        if (__P.isType ~= "object") or not vcl.private.isVoid(char) then
-            if __P.isType == "object" then
-                __P.index = __P.index..char
+        if (__p.isType ~= "object") or not vcl.private.isVoid(char) then
+            if __p.isType == "object" then
+                __p.index = __p.index..char
                 local __char = vcl.private.fetch(buffer, index + 1)
                 if __char and (__char == ":") then
                     local value, __index = vcl.private.parse(buffer, index + 2, true)
                     if value then
-                        __P.pointer[(__P.index)], index = value, __index
-                        __P.index = ""
+                        __p.pointer[(__p.index)], index = value, __index
+                        __p.index = ""
                     else
-                        __P.isChildErrored = true
+                        __p.isChildErrored = true
                         break
                     end
                 end
             else
                 local isSkipAppend = false
-                if not __P.isType or (__P.isType == "object") then
+                if not __p.isType or (__p.isType == "object") then
                     --TODO: CHECK IF ITS OVJECT???
                     --[[
                     if (char == "\"") or (char == "\'") then
-                        if not __P.isType then
-                            isSkipAppend, __P.isType = true, "object"
+                        if not __p.isType then
+                            isSkipAppend, __p.isType = true, "object"
                         else
-                            __P.isParsed = true
+                            __p.isParsed = true
                         end
                     end
                     ]]
                 end
-                if not __P.isType or (__P.isType == "string") then
-                    if (not __P.isTypeChar and ((char == "\"") or (char == "\'"))) or (__P.isTypeChar and (__P.isTypeChar == char)) then
-                        if not __P.isType then isSkipAppend, __P.isType, __P.isTypeChar = true, "string", char
-                        else __P.isParsed = true end
+                if not __p.isType or (__p.isType == "string") then
+                    if (not __p.isTypeChar and ((char == "\"") or (char == "\'"))) or (__p.isTypeChar and (__p.isTypeChar == char)) then
+                        if not __p.isType then isSkipAppend, __p.isType, __p.isTypeChar = true, "string", char
+                        else __p.isParsed = true end
                     end
                 end
-                if not __P.isType or (__P.isType == "number") then
+                if not __p.isType or (__p.isType == "number") then
                     local isNumber = imports.tonumber(char)
-                    if not __P.isType and isNumber then __P.isType = "number"
-                    elseif __P.isType then
+                    if not __p.isType and isNumber then __p.isType = "number"
+                    elseif __p.isType then
                         if char == "." then
-                            if not __P.isTypeFloat then __P.isTypeFloat = true
+                            if not __p.isTypeFloat then __p.isTypeFloat = true
                             else break end
-                        elseif (char == " ") or (char == "\n") then __P.isParsed = true
+                        elseif (char == " ") or (char == "\n") then __p.isParsed = true
                         elseif not isNumber then break end
                     end
                 end
-                if __P.isType and not isSkipAppend and not __P.isParsed then __P.value = __P.value..char end
+                if __p.isType and not isSkipAppend and not __p.isParsed then __p.value = __p.value..char end
             end
-        elseif (__P.isType == "object") and not vcl.private.isVoid(__P.index) then
-            __P.isParsed = false
+        elseif (__p.isType == "object") and not vcl.private.isVoid(__p.index) then
+            __p.isParsed = false
             break
         end
         index = index + 1
-        if isChild and __P.isParsed then break end
+        if isChild and __p.isParsed then break end
     end
-    __P.isParsed = (not __P.isChildErrored and __P.isParsed) or __P.isParsed
-    if not __P.isParsed then
-        if not __P.isChildErrored then
-            __P.isErrored = string.format(
-                __P.isErrored,
-                vcl.private.fetchLine(buffer, __P.ref or index),
-                ((__P.isType == "string") and "Unterminated string") or
-                ((__P.isType == "number") and "Malformed number") or
+    __p.isParsed = (not __p.isChildErrored and __p.isParsed) or __p.isParsed
+    if not __p.isParsed then
+        if not __p.isChildErrored then
+            __p.isErrored = string.format(
+                __p.isErrored,
+                vcl.private.fetchLine(buffer, __p.ref or index),
+                ((__p.isType == "string") and "Unterminated string") or
+                ((__p.isType == "number") and "Malformed number") or
                 "Invalid declaration"
             )
-            imports.outputDebugString(__P.isErrored)
+            imports.outputDebugString(__p.isErrored)
         end
-        return __P.isParsed, false, __P.isErrored
-    elseif (__P.isType == "object") then return __P.pointer, index
-    else return ((__P.isType == "number" and imports.tonumber(__P.value)) or __P.value), index end
+        return __p.isParsed, false, __p.isErrored
+    elseif (__p.isType == "object") then return __p.pointer, index
+    else return ((__p.isType == "number" and imports.tonumber(__p.value)) or __p.value), index end
 end
 
 vcl.public.parse = function(buffer)
