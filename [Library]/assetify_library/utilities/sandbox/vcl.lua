d9e40a2cf4452b31c6e73770a9ab82af26eb15d4
Aviril <48655846+OvileAmriam@users.noreply.github.com>
2022-08-24 18:48:12 +0300

    Update vcl.lua

--- [Library]/assetify_library/utilities/sandbox/vcl.lua
+++ [Library]/assetify_library/utilities/sandbox/vcl.lua
@@ -41,7 +41,7 @@ function vcl.private.parseComment(parser, buffer, rw)
     local isCommentLine = parser.isCommentLine
     parser.isCommentLine = vcl.private.fetchLine(buffer, parser.ref)
     parser.isComment = ((isCommentLine == parser.isCommentLine) and parser.isComment) or false
-    parser.isComment = ((not parser.isType or vcl.private.isVoid(parser.index)) and not parser.isComment and (rw == "#") and true) or parser.isComment
+    parser.isComment = (not parser.isComment and (not parser.isType or vcl.private.isVoid(parser.index)) and not parser.isComment and (rw == "#") and true) or parser.isComment
     return true
 end
 
