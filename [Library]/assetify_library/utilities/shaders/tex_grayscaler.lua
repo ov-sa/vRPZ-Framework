550f9707cae460bfde6327d3f7584600c7aecbb2
Aviril <89867985+Aviril@users.noreply.github.com>
2022-08-29 23:21:30 +0300

    Merged tex_grayscaler update

--- [Library]/assetify_library/utilities/shaders/tex_grayscaler.lua
+++ [Library]/assetify_library/utilities/shaders/tex_grayscaler.lua
@@ -8,23 +8,12 @@
 ----------------------------------------------------------------
 
 
--------------------
---[[ Variables ]]--
--------------------
-
-local identity = {
-    name = "Assetify_TextureGrayscaler",
-    deps = shaderRW.createDeps({
-        "utilities/shaders/helper.fx"
-    })
-}
-
-
 ----------------
 --[[ Shader ]]--
 ----------------
 
-shaderRW.buffer[(identity.name)] = {
+local identity = "Assetify_TextureGrayscaler"
+shaderRW.buffer[identity] = {
     properties = {
         disabled = {
             ["vSource0"] = true,
@@ -34,7 +23,7 @@ shaderRW.buffer[(identity.name)] = {
     },
 
     exec = function()
-        return identity.deps..[[
+        return shaderRW.createHelper()..[[
         /*-----------------
         -->> Variables <<--
         -------------------*/
@@ -69,7 +58,7 @@ shaderRW.buffer[(identity.name)] = {
         -->> Techniques <<--
         --------------------*/
 
-        technique ]]..identity.name..[[ {
+        technique ]]..identity..[[ {
             pass P0 {
                 AlphaRef = 1;
                 AlphaBlendEnable = true;
