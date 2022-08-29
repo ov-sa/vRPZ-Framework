3147ae369d95d71e54a0044cfe7c258bb9365733
Aviril <103286142+Anisa-Nur@users.noreply.github.com>
2022-08-29 19:59:34 +0300

    Update tex_sampler.lua

--- [Library]/assetify_library/utilities/shaders/tex_sampler.lua
+++ [Library]/assetify_library/utilities/shaders/tex_sampler.lua
@@ -210,10 +210,10 @@ shaderRW.buffer[(identity.name)] = {
             float viewPI = PI*2;
             float2 viewRadius = 20/vResolution;
             float viewIterations = 26, viewQuality = 4, viewBrightness = 1.5;
-            float4 result = tex2D(vSource0Sampler, float4(uv, 0, 0))*viewBrightness;
+            float4 result = tex2Dlod(vSource0Sampler, float4(uv, 0, 0))*viewBrightness;
             for(float i = 0; i < viewPI; i += viewPI/viewIterations) {
                 for(float j = 1/viewQuality; j <= 1; j += 1/viewQuality) {
-                    result += tex2D(vSource0Sampler, float4(uv + (float2(cos(i), sin(i))*viewRadius*j), 0, 0))*viewBrightness;
+                    result += tex2Dlod(vSource0Sampler, float4(uv + (float2(cos(i), sin(i))*viewRadius*j), 0, 0))*viewBrightness;
                 }
             }
             result /= (viewQuality*viewIterations) - 15;
